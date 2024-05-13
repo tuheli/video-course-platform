3cf93cf1a2ea0c6f21595cdb6e907631c5d96447
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-14 01:35:01 +0300

    Create LoginButton.tsx

---
+++ src/components/appbar/LoginButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+export const LoginButton = () => {
+  const onClickLogin = () => {};
+
+  return (
+    <Button variant="outlined" onClick={onClickLogin}>
+      Log in
+    </Button>
+  );
+};
