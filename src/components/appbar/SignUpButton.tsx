fe2e2d12212f2369a19745b2d66b28fb832b9b9a
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-14 01:34:59 +0300

    Create SignUpButton.tsx

---
+++ src/components/appbar/SignUpButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+export const SignUpButton = () => {
+  const onClickSignUp = () => {};
+
+  return (
+    <Button variant="contained" color="primary" onClick={onClickSignUp}>
+      Sign up
+    </Button>
+  );
+};
