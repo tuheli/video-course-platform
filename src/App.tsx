729683af51972a233e12bb7fe68914c7fe50b425
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-14 01:35:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import {
-  CssBaseline,
-  PaletteMode,
-  ThemeProvider,
-  createTheme,
-} from '@mui/material';
-import LandingPage from './components/landing-page/LandingPage';
-import getTheme from './theme';
 import { useState } from 'react';
+import getTheme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { PaletteMode, createTheme } from '@mui/material';
+import LandingPage from './components/landing-page/LandingPage';
+
+// NOTE: Import css baseline up top to prevent mui error that somethimes occurs.
 
 const App = () => {
   const [themeMode, setThemeMode] = useState<PaletteMode>('light');
