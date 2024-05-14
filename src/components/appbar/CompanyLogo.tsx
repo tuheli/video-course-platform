be7a309be8d7e68468dbb8430c2b9eb962acaa0b
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-14 14:17:19 +0300

    Create CompanyLogo.tsx

---
+++ src/components/appbar/CompanyLogo.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material';
+
+export const CompanyLogo = () => {
+  return (
+    <a
+      href="/"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        component="img"
+        src="/company-logos/logoipsum-317.svg"
+        sx={{
+          maxHeight: '1.3rem',
+          cursor: 'pointer',
+        }}
+      ></Box>
+    </a>
+  );
+};
