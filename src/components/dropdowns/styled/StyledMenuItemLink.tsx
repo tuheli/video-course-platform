e413d26dee7956c292c0b86c479becceb0cdfdc0
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-16 11:35:45 +0300

    Update StyledMenuItemLink.tsx

--- src/components/dropdowns/styled/StyledMenuItemLink.tsx
+++ src/components/dropdowns/styled/StyledMenuItemLink.tsx
@@ -3,7 +3,6 @@ import { styled } from '@mui/system';
 export const StyledMenuItemLink = styled('a')({
   display: 'flex',
   alignItems: 'center',
-  padding: '8px 16px',
   textDecoration: 'none',
   '&:hover': {
     color: 'secondary.main',
