3efe5b5137b44eeb710bb2a0582c8f99101e80ae
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-16 15:48:21 +0300

    Create BestSeller.tsx

---
+++ src/components/broad-courses-selection/BestSeller.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material';
+
+export const BestSeller = () => {
+  return (
+    <Typography
+      sx={{
+        display: 'inline-block',
+        backgroundColor: 'background.secondary',
+        fontSize: 11,
+        fontWeight: 600,
+        p: 0.5,
+      }}
+    >
+      Bestseller
+    </Typography>
+  );
+};
