d886dd20d1fd7e44dbce2925bd0385f851b4c17f
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-16 19:11:24 +0300

    Added comment on a bug

--- src/components/dropdowns/dropdown-multi-level/CategoriesDropdown.tsx
+++ src/components/dropdowns/dropdown-multi-level/CategoriesDropdown.tsx
@@ -3,6 +3,8 @@ import { MenuItems } from './MenuItems';
 import { OuterMenuItemsList } from '../styled/OuterMenuItemsList';
 import { TextDropdownOpener } from '../dropdown-openers/TextDropdownOpener';
 
+// FIX: I noticed a bug when zooming out enough on the page. Error margin increases in positioning the dropdown submenus and the hovering wont work properly. Potential fix is to use similiar positioning as in the newer broad selection of courses popover card.
+
 export const CategoriesDropdown = () => {
   return (
     <TextDropdownOpener text="Categories">
