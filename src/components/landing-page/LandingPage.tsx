6ab520811a125369b35a6191d86b22a5208fbca3
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-18 21:38:27 +0300

    Update LandingPage.tsx

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -3,6 +3,8 @@ import AppAppBar from '../appbar/AppAppBar';
 import { HeroBanner } from '../hero-banner/HeroBanner';
 import { TrustedByCompaniesLogos } from '../trusted-by-companies-logos/TrustedByCompaniesLogos';
 import { BroadCoursesSelection } from '../broad-courses-selection/BroadCoursesSelection';
+import { LearnerPositiveReviews } from '../learner-positive-reviews/LearnerPositiveReviews';
+import { LearnersAreViewingSlider } from '../learners-are-viewing-slider/LearnersAreViewingSlider';
 
 const LandingPage = () => {
   return (
@@ -16,7 +18,28 @@ const LandingPage = () => {
       >
         <TrustedByCompaniesLogos />
       </Box>
-      <BroadCoursesSelection />
+      <Box
+        sx={{
+          py: 4,
+        }}
+      >
+        <BroadCoursesSelection />
+      </Box>
+      <Box
+        sx={{
+          py: 4,
+          bgcolor: 'background.paper',
+        }}
+      >
+        <LearnerPositiveReviews />
+      </Box>
+      <Box
+        sx={{
+          py: 4,
+        }}
+      >
+        <LearnersAreViewingSlider />
+      </Box>
     </>
   );
 };
