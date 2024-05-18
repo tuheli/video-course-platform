0dfac8df5e7a7430d5d9e6bc00e54e49787adf62
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-19 01:48:58 +0300

    Added img scaling animation and cursor change on hover

--- src/components/top-categories/TopCategoryItem.tsx
+++ src/components/top-categories/TopCategoryItem.tsx
@@ -1,20 +1,44 @@
 import { Box, Stack, Typography } from '@mui/material';
 import { CourseCategory } from '../broad-courses-selection/broadCoursesSelectionData';
+import { StyledLink } from '../learner-positive-reviews/StyledLink';
 
 interface TopCategoryItemProps {
   category: CourseCategory;
 }
 
 export const TopCategoryItem = ({ category }: TopCategoryItemProps) => {
+  const onClick = () => {};
+
   return (
-    <Stack
-      sx={{
-        flexDirection: 'column',
-        gap: 1,
-      }}
-    >
-      <Box component="img" src={category.imageUrl} />
-      <Typography variant="h6">{category.name}</Typography>
-    </Stack>
+    <StyledLink onClick={onClick}>
+      <Stack
+        sx={{
+          flexDirection: 'column',
+          gap: 1,
+        }}
+      >
+        <Box
+          sx={{
+            overflow: 'hidden',
+          }}
+        >
+          <Box
+            component="img"
+            src={category.imageUrl}
+            sx={{
+              width: '100%',
+              height: 'auto',
+              display: 'block',
+              transform: 'scale(1)',
+              transition: 'transform 0.2s ease-in-out',
+              '&:hover': {
+                transform: 'scale(1.1)',
+              },
+            }}
+          />
+        </Box>
+        <Typography variant="h6">{category.name}</Typography>
+      </Stack>
+    </StyledLink>
   );
 };
