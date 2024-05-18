41fa4c801d743d14a0e3b94f07032354861a49f6
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-19 00:31:55 +0300

    App scrolls to top on effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import getTheme from './theme';
 import CssBaseline from '@mui/material/CssBaseline';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { PaletteMode, createTheme } from '@mui/material';
 import LandingPage from './components/landing-page/LandingPage';
 
-// NOTE: Import css baseline up top to prevent mui error that somethimes occurs.
+// NOTE: Import css baseline up top to prevent mui error that somethimes occurs
+
+// NOTE: Scrolling to top on initial load prevents visual flickering if window was scrolled down
 
 const App = () => {
   const [themeMode] = useState<PaletteMode>('light');
   const theme = createTheme(getTheme(themeMode));
+
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
