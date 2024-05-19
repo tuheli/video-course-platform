35f32aa368d13d0a96408d9eb824182b587907e3
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-19 23:34:00 +0300

    Create TopicsByCategoryStack.tsx

---
+++ src/components/featured-topics-by-category/TopicsByCategoryStack.tsx
@@ -0,0 +1,53 @@
+import { Stack, Typography } from '@mui/material';
+import { CourseCategory, getFeaturedTopics } from '../../../data/courseData';
+import { format } from '../../utils/numberFormatters';
+import { StyledLink } from './StyledLink';
+
+interface TopicsByCategoryStackProps {
+  category: CourseCategory;
+}
+
+export const TopicsByCategoryStack = ({
+  category,
+}: TopicsByCategoryStackProps) => {
+  const featuredTopics = getFeaturedTopics(category.name);
+  const onClickLink = () => {};
+
+  return (
+    <Stack
+      sx={{
+        flexDirection: 'column',
+        gap: 2,
+        flex: 1,
+      }}
+    >
+      <Typography variant="h6">{category.name}</Typography>
+      <Stack
+        sx={{
+          flexDirection: 'column',
+          gap: 2,
+        }}
+      >
+        {featuredTopics.map((topic, index) => {
+          const learnerCount = topic.courses.reduce((acc, course) => {
+            return acc + course.learerCount;
+          }, 0);
+
+          return (
+            <Stack
+              key={index}
+              sx={{
+                gap: 0.5,
+              }}
+            >
+              <StyledLink onClick={onClickLink}>{topic.name}</StyledLink>
+              <Typography variant="caption">
+                {format(learnerCount)} learners
+              </Typography>
+            </Stack>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
+};
