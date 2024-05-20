f902af10cd57683b7ad5f82f844fae01df714e4a
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-20 14:17:37 +0300

    Updated text underline offset

--- src/components/featured-topics-by-category/StyledLink.tsx
+++ src/components/featured-topics-by-category/StyledLink.tsx
@@ -4,6 +4,7 @@ export const StyledLink = styled('a')(({ theme }) => ({
   color: theme.palette.secondary.main,
   fontWeight: 600,
   textDecoration: 'underline',
+  textUnderlineOffset: 4,
   '&:hover': {
     cursor: 'pointer',
   },
