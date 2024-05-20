9a5272490a2fb4870afa8862eb2829879cad0e3c
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-20 15:51:43 +0300

    Language data no longer needs to extend column ordered grid item

--- data/languageData.ts
+++ data/languageData.ts
@@ -1,6 +1,15 @@
-import { ColumOrderedGridItem } from '../src/components/column-ordered-grid/ColumnOrderedGrid';
+import { ColumnOrderedGridItem } from '../src/components/column-ordered-grid/ColumnOrderedGrid';
 
-export const languages: ColumOrderedGridItem[] = [
+interface Language {
+  key: string;
+  text: string;
+}
+
+export const getLanguages = () => {
+  return languages;
+};
+
+const languages: Language[] = [
   { key: 'en', text: 'English' },
   { key: 'es', text: 'Spanish' },
   { key: 'fr', text: 'French' },
