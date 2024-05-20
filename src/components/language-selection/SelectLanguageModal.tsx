7eeb331f5a0414ec6d482a304dc17b0e15c6cfd6
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-20 15:51:04 +0300

    Updated for new column ordered grid props

--- src/components/language-selection/SelectLanguageModal.tsx
+++ src/components/language-selection/SelectLanguageModal.tsx
@@ -7,9 +7,10 @@ import {
   Typography,
 } from '@mui/material';
 import { ColumnOrderedGrid } from '../column-ordered-grid/ColumnOrderedGrid';
-import { languages } from '../../../data/languageData';
 import CloseIcon from '@mui/icons-material/Close';
 import { useDisableScrollbar } from '../../hooks/useDisableScrollbar';
+import { getLanguages } from '../../../data/languageData';
+import { LanguageRenderComponent } from './LanguageRenderComponent';
 
 // NOTE: I removed fade animation wrapper to prevent seeing a change in document padding when modal is still fading away but scrollbar becomes visible and extra document body padding is removed.
 
@@ -23,9 +24,14 @@ export const SelectLanguageModal = ({
   onClose,
 }: SelectLanguageModalProps) => {
   // When used add item: ColumOrderedGridItem as an argument to use the items data in this function. I removed it to silence unused argument warning.
-  const onClickLanguage = () => {
+  const closeModal = () => {
     onClose();
   };
+
+  const languages = getLanguages();
+  const languageComponents = languages.map(({ text }) => ({
+    RenderComponent: () => <LanguageRenderComponent language={text} />,
+  }));
 
   useDisableScrollbar(isOpen);
 
@@ -61,9 +67,7 @@ export const SelectLanguageModal = ({
               pb: 1.5,
             }}
           >
-            <Typography variant="h5" sx={{}}>
-              Choose a language
-            </Typography>
+            <Typography variant="h5">Choose a language</Typography>
             <IconButton
               onClick={onClose}
               sx={{
@@ -83,9 +87,9 @@ export const SelectLanguageModal = ({
             </IconButton>
           </Stack>
           <ColumnOrderedGrid
-            items={languages}
+            items={languageComponents}
             stackHeight={4}
-            onClickItem={onClickLanguage}
+            onClickItem={closeModal}
           />
         </Box>
       </>
