7570ff3b5a7cd011f671c8b909fa45150e3b64eb
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-21 18:55:03 +0300

    Added link to teaching page

--- src/components/become-an-instructor/BecomeAnInstructor.tsx
+++ src/components/become-an-instructor/BecomeAnInstructor.tsx
@@ -1,10 +1,9 @@
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const itemWidth = 360;
 
 export const BecomeAnInstructor = () => {
-  const onClickStartTeachingToday = () => {};
-
   return (
     <Container>
       <Stack
@@ -46,13 +45,11 @@ export const BecomeAnInstructor = () => {
             Udemy. We provide the tools and skills to teach what you love.
           </Typography>
           <Box>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={onClickStartTeachingToday}
-            >
-              Start teaching today
-            </Button>
+            <Link to="/teaching">
+              <Button variant="contained" color="primary">
+                Start teaching today
+              </Button>
+            </Link>
           </Box>
         </Stack>
       </Stack>
