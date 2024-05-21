0acd25f364a350394c3e37b6aca75314f4c48039
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-21 17:54:32 +0300

    Using router link instead of a tag

--- src/components/dropdowns/styled/StyledMenuItemLink.tsx
+++ src/components/dropdowns/styled/StyledMenuItemLink.tsx
@@ -1,6 +1,7 @@
 import { styled } from '@mui/system';
+import { Link } from 'react-router-dom';
 
-export const StyledMenuItemLink = styled('a')({
+export const StyledMenuItemLink = styled(Link)({
   display: 'flex',
   alignItems: 'center',
   textDecoration: 'none',
