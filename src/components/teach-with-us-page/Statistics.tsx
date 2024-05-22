547460cd26f3fb29fbbf04f3f80d4377c7d587f8
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-22 10:33:03 +0300

    Updated layout

--- src/components/teach-with-us-page/Statistics.tsx
+++ src/components/teach-with-us-page/Statistics.tsx
@@ -1,19 +1,21 @@
-import { Stack } from '@mui/material';
+import { Container, Stack } from '@mui/material';
 import { StatisticsItem } from './StatisticsItem';
 
 export const Statistics = () => {
   return (
-    <Stack
-      sx={{
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-      }}
-    >
-      <StatisticsItem count="62M" text="Students" />
-      <StatisticsItem count="75+" text="Languages" />
-      <StatisticsItem count="830M" text="Enrollments" />
-      <StatisticsItem count="180+" text="Countries" />
-      <StatisticsItem count="15,000+" text="Enterprise customers" />
-    </Stack>
+    <Container>
+      <Stack
+        sx={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <StatisticsItem count="62M" text="Students" />
+        <StatisticsItem count="75+" text="Languages" />
+        <StatisticsItem count="830M" text="Enrollments" />
+        <StatisticsItem count="180+" text="Countries" />
+        <StatisticsItem count="15,000+" text="Enterprise customers" />
+      </Stack>
+    </Container>
   );
 };
