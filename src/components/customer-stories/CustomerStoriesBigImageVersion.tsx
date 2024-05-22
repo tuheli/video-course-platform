bb3413f38b91ac219dbf1cb5f14fd51c6784d2b4
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-22 15:18:18 +0300

    Create CustomerStoriesBigImageVersion.tsx

---
+++ src/components/customer-stories/CustomerStoriesBigImageVersion.tsx
@@ -0,0 +1,63 @@
+import { Box, Container } from '@mui/material';
+import { ArrowRight } from '../scroll-to-view-list/ArrowRight';
+import { ArrowLeft } from '../scroll-to-view-list/ArrowLeft';
+import { getCustomerStoriesBigImageVersion } from '../../../data/customerStoriesData';
+import { CustomerStoryCardBigImageVersion } from './CustomerStoryCardBigImageVersion';
+import Slider from 'react-slick';
+
+const slidesToShowCount = 1;
+
+export const CustomerStoriesBigImageVersion = () => {
+  const customerStories = getCustomerStoriesBigImageVersion();
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          maxWidth: 740,
+          margin: 'auto',
+        }}
+      >
+        <Slider
+          {...{
+            infinite: false,
+            touchMove: false,
+            speed: 500,
+            slidesToShow: slidesToShowCount,
+            slidesToScroll: 1,
+            nextArrow: (
+              <ArrowRight
+                isSliderInfinite={false}
+                slidesToShow={slidesToShowCount}
+                sx={{
+                  right: -30,
+                  top: '50%',
+                  transform: 'translateY(-50%) translateX(50%)',
+                }}
+              />
+            ),
+            prevArrow: (
+              <ArrowLeft
+                isSliderInfinite={false}
+                sx={{
+                  left: -30,
+                  top: '50%',
+                  transform: 'translateY(-50%) translateX(-50%)',
+                }}
+              />
+            ),
+          }}
+        >
+          {customerStories.map((customerStory, index) => {
+            return (
+              <CustomerStoryCardBigImageVersion
+                key={index}
+                customerStory={customerStory}
+              />
+            );
+          })}
+        </Slider>
+      </Box>
+    </Container>
+  );
+};
