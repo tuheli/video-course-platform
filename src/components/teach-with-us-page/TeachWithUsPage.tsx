62f6967b20146ddb7e775f2541cb66c44ffb9573
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-22 16:20:05 +0300

    Layout updates, added new sections

--- src/components/teach-with-us-page/TeachWithUsPage.tsx
+++ src/components/teach-with-us-page/TeachWithUsPage.tsx
@@ -5,8 +5,14 @@ import { Statistics } from './Statistics';
 import { Banner } from './Banner';
 import { HowToBegin } from './HowToBegin';
 import { CustomerStoriesBigImageVersion } from '../customer-stories/CustomerStoriesBigImageVersion';
+import { Footer } from '../footer/Footer';
+import { BecomeAnInstructorVariant } from '../become-an-instructor/BecomeAnInstructorVariant';
+import { YouWontHaveToDoItAlone } from './YouWontHaveToDoItAlone';
+import { useScrollToTop } from '../../hooks/useScrollToTop';
 
 export const TeachWithUsPage = () => {
+  useScrollToTop();
+
   return (
     <>
       <AppAppBar />
@@ -35,7 +41,7 @@ export const TeachWithUsPage = () => {
       </Box>
       <Box
         sx={{
-          py: 12,
+          py: 6,
         }}
       >
         <Box
@@ -45,6 +51,29 @@ export const TeachWithUsPage = () => {
         >
           <CustomerStoriesBigImageVersion />
         </Box>
+      </Box>
+      <Box
+        sx={{
+          py: 6,
+        }}
+      >
+        <YouWontHaveToDoItAlone />
+      </Box>
+      <Box
+        sx={{
+          py: 8,
+          bgcolor: 'background.paper',
+        }}
+      >
+        <BecomeAnInstructorVariant />
+      </Box>
+      <Box
+        sx={{
+          py: 4,
+          bgcolor: 'background.dark',
+        }}
+      >
+        <Footer />
       </Box>
     </>
   );
