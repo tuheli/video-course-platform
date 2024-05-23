cb3e5d482b71a680865217b6ee74c8cfa0abcf66
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-23 12:31:54 +0300

    Using signed in status from redux  to switch between routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,22 @@ import getTheme from './theme';
 import CssBaseline from '@mui/material/CssBaseline';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { PaletteMode, createTheme } from '@mui/material';
-import LandingPage from './components/landing-page/LandingPage';
-import { Route, Routes } from 'react-router-dom';
-import { NotFoundPage } from './components/not-found-page/NotFoundPage';
-import { TeachWithUsPage } from './components/teach-with-us-page/TeachWithUsPage';
+import { SignedOutRoutes } from './components/routes/SignedOutRoutes';
+import { useAppSelector } from './app/hooks';
+import { SignedInRoutes } from './components/routes/SignedInRoutes';
 
 // NOTE: Import css baseline up top to prevent mui error that somethimes occurs
 
-// NOTE: Scrolling to top on initial load prevents visual flickering if window was scrolled down
-
 const App = () => {
+  const me = useAppSelector((state) => state.me.user);
   const [themeMode] = useState<PaletteMode>('light');
   const theme = createTheme(getTheme(themeMode));
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/teaching" element={<TeachWithUsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      {!me && <SignedOutRoutes />}
+      {me && <SignedInRoutes />}
     </ThemeProvider>
   );
 };
