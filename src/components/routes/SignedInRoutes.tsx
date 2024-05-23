70f6f7e16c25127af705e44f5198757e60a7048c
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-23 18:03:08 +0300

    Added route to instructor courses page

--- src/components/routes/SignedInRoutes.tsx
+++ src/components/routes/SignedInRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../landing-page/LandingPage';
 import { NotFoundPage } from '../not-found-page/NotFoundPage';
 import { Redirect } from '../redirect/Redirect';
+import { InstructorCoursesPage } from '../instructor/InstructorCoursesPage';
 
 // NOTE: The signed in front page is quite different from landing page so for that it would be good to create a separate page component using some of the same sections as in the landing page.
 
@@ -9,6 +10,11 @@ export const SignedInRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/instructor/courses" element={<InstructorCoursesPage />} />
+      <Route
+        path="/instructor"
+        element={<Redirect to="/instructor/courses" />}
+      />
       <Route path="/teaching" element={<Redirect to="/" />} />
       <Route path="/signup" element={<Redirect to="/" />} />
       <Route path="*" element={<NotFoundPage />} />
