76fd9406c091e8ceba2d445a93ae00c8eb348c6e
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-23 12:28:59 +0300

    Create SignUpSection.tsx

---
+++ src/components/sign-up/SignUpSection.tsx
@@ -0,0 +1,17 @@
+import { Box, Container } from '@mui/material';
+import { SignUpForm } from './SignUpForm';
+
+export const SignUpSection = () => {
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <SignUpForm />
+      </Box>
+    </Container>
+  );
+};
