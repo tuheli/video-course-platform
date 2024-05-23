b38fce8b0ced6af17d1a3fed30ca4e28935baed2
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-23 17:57:21 +0300

    Create UserAvatar.tsx

---
+++ src/components/me/UserAvatar.tsx
@@ -0,0 +1,54 @@
+import { Avatar, Box, SxProps } from '@mui/material';
+import { useAppSelector } from '../../app/hooks';
+
+interface AvatarLetters {
+  firstLetter?: string;
+  secondLetter?: string;
+}
+
+const getAvatarLetters = (fullName: string): AvatarLetters => {
+  const names = fullName.split(' ');
+
+  if (names.length === 1) {
+    return {
+      firstLetter: names[0][0].toUpperCase(),
+    };
+  }
+
+  if (names.length > 1) {
+    return {
+      firstLetter: names[0][0].toUpperCase(),
+      secondLetter: names[names.length - 1][0],
+    };
+  }
+
+  return {
+    firstLetter: undefined,
+    secondLetter: undefined,
+  };
+};
+
+interface UserAvatarProps {
+  sx?: SxProps;
+}
+
+export const UserAvatar = ({ sx }: UserAvatarProps) => {
+  const me = useAppSelector((state) => state.me.user);
+
+  const avatarLetters = me ? getAvatarLetters(me.fullName) : {};
+
+  return (
+    <Box
+      sx={{
+        '&:hover': {
+          cursor: 'pointer',
+        },
+      }}
+    >
+      <Avatar sx={{ ...sx }}>
+        {avatarLetters.firstLetter}
+        {avatarLetters.secondLetter}
+      </Avatar>
+    </Box>
+  );
+};
