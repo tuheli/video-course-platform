52d7f9971f1c0360a25d5e52e968f9e2d8f8b6e8
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-24 11:23:01 +0300

    Create HelpfulResourcesHeader.tsx

---
+++ src/components/instructor/HelpfulResourcesHeader.tsx
@@ -0,0 +1,15 @@
+import { Container, Typography } from '@mui/material';
+
+export const HelpfulResourcesHeader = () => {
+  return (
+    <Container>
+      <Typography
+        sx={{
+          textAlign: 'center',
+        }}
+      >
+        Based on your experience, we think these resources will be helpful.
+      </Typography>
+    </Container>
+  );
+};
