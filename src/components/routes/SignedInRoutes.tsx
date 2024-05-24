55deea687d88c58aea93d75b2750e9676af8c492
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-24 17:55:57 +0300

    Added course creation page and redirector to it

--- src/components/routes/SignedInRoutes.tsx
+++ src/components/routes/SignedInRoutes.tsx
@@ -3,6 +3,7 @@ import LandingPage from '../landing-page/LandingPage';
 import { NotFoundPage } from '../not-found-page/NotFoundPage';
 import { Redirect } from '../redirect/Redirect';
 import { InstructorCoursesPage } from '../instructor/InstructorCoursesPage';
+import { CourseCreationPage } from '../course-creation/CourseCreationPage';
 
 // NOTE: The signed in front page is quite different from landing page so for that it would be good to create a separate page component using some of the same sections as in the landing page.
 
@@ -14,6 +15,11 @@ export const SignedInRoutes = () => {
       <Route
         path="/instructor"
         element={<Redirect to="/instructor/courses" />}
+      />
+      <Route path="/course/create/:step" element={<CourseCreationPage />} />
+      <Route
+        path="/course/create"
+        element={<Redirect to="/course/create/1" />}
       />
       <Route path="/teaching" element={<Redirect to="/" />} />
       <Route path="/signup" element={<Redirect to="/" />} />
