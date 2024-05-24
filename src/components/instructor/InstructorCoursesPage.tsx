95cc870cb472c9af9aeb670db20109ed97953480
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-24 11:30:05 +0300

    Added new sections

--- src/components/instructor/InstructorCoursesPage.tsx
+++ src/components/instructor/InstructorCoursesPage.tsx
@@ -1,9 +1,61 @@
+import { Box } from '@mui/material';
 import { InstructorAppBar } from './InstructorAppBar';
+import { JumpIntoCourseCreation } from '../course-creation/JumpIntoCourseCreation';
+import { HelpfulResourcesHeader } from './HelpfulResourcesHeader';
+import { CreateAnEngagingCourse } from '../create-an-engaging-course/CreateAnEngagingCourse';
+import { PopularInstructorResources } from '../popular-instructor-resources/PopularInstructorResources';
+import { AreYouReadyToBegin } from './AreYouReadyToBegin';
+import { Footer } from '../footer/Footer';
 
 export const InstructorCoursesPage = () => {
   return (
     <>
       <InstructorAppBar />
+      <Box
+        sx={{
+          pt: 8,
+          pb: 4,
+        }}
+      >
+        <JumpIntoCourseCreation />
+      </Box>
+      <Box
+        sx={{
+          pt: 4,
+        }}
+      >
+        <HelpfulResourcesHeader />
+      </Box>
+      <Box
+        sx={{
+          pt: 8,
+        }}
+      >
+        <CreateAnEngagingCourse />
+      </Box>
+      <Box
+        sx={{
+          py: 8,
+        }}
+      >
+        <PopularInstructorResources />
+      </Box>
+      <Box
+        sx={{
+          pt: 8,
+          pb: 16,
+        }}
+      >
+        <AreYouReadyToBegin />
+      </Box>
+      <Box
+        sx={{
+          py: 4,
+          bgcolor: 'background.dark',
+        }}
+      >
+        <Footer />
+      </Box>
     </>
   );
 };
