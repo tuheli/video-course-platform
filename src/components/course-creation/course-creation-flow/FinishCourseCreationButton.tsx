fa98844ce34c03f061c32aed26f902a647836611
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-25 14:15:28 +0300

    Course draft can now be created

--- src/components/course-creation/course-creation-flow/FinishCourseCreationButton.tsx
+++ src/components/course-creation/course-creation-flow/FinishCourseCreationButton.tsx
@@ -1,16 +1,66 @@
 import { Button } from '@mui/material';
 import { stepButtonWidth } from './change-step-buttons/common';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import {
+  NewCourseDraftEntry,
+  createdCourseDraft,
+} from '../../../features/courseDraftsSlice';
+import { useNavigate } from 'react-router-dom';
 
-export const FinishCourseCreationButton = () => {
+interface FinishCourseCreationButtonProps {
+  isAbleToFinish: boolean;
+}
+
+export const FinishCourseCreationButton = ({
+  isAbleToFinish,
+}: FinishCourseCreationButtonProps) => {
+  const creatorEmail = useAppSelector(
+    (state) => state.me.user?.credentials.email
+  );
+  const { step1, step2, step3, step4 } = useAppSelector(
+    (state) => state.courseCreation.steps
+  );
+  const dispatch = useAppDispatch();
+  const navigage = useNavigate();
+
+  const onClickFinish = () => {
+    if (!isAbleToFinish) {
+      return;
+    }
+
+    if (
+      !creatorEmail ||
+      !step1.courseType ||
+      !step2.title ||
+      !step3.category ||
+      !step4.timeAvailablePerWeek
+    ) {
+      return;
+    }
+
+    const newEntry: NewCourseDraftEntry = {
+      creatorEmail,
+      courseType: step1.courseType,
+      courseTitle: step2.title,
+      courseCategory: step3.category,
+      creatorTimeAvailablePerWeek: step4.timeAvailablePerWeek,
+    };
+
+    dispatch(createdCourseDraft(newEntry));
+    navigage('/instructor');
+  };
+
   return (
     <Button
+      disabled={!isAbleToFinish}
       variant="contained"
       color="primary"
       sx={{
-        width: stepButtonWidth,
+        minWidth: stepButtonWidth,
       }}
+      onClick={onClickFinish}
     >
-      Finish
+      Create Course
     </Button>
   );
 };
