b10d98c696da4c27a0279b751814e09b99db1699
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-27 12:57:28 +0300

    Create CourseDraftItems.tsx

---
+++ src/components/instructor/coursedrafts/CourseDraftItems.tsx
@@ -0,0 +1,19 @@
+import { useAppSelector } from '../../../app/hooks';
+import { CourseDraftItem } from './CourseDraftItem';
+
+export const CourseDraftItems = () => {
+  const myEmail = useAppSelector((state) => state.me.user?.credentials.email);
+  const courseDrafts = useAppSelector((state) => state.courseDrafts);
+
+  const myCourseDrafts = courseDrafts.filter(
+    ({ creatorEmail }) => creatorEmail === myEmail
+  );
+
+  return (
+    <>
+      {myCourseDrafts.map((courseDraft) => (
+        <CourseDraftItem courseDraft={courseDraft} />
+      ))}
+    </>
+  );
+};
