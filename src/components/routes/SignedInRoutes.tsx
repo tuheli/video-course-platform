6ebd5a9cb59ffa9716eece55a6d4a8fb168a0840
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-27 13:37:18 +0300

    Added comments

--- src/components/routes/SignedInRoutes.tsx
+++ src/components/routes/SignedInRoutes.tsx
@@ -5,24 +5,33 @@ import { Redirect } from '../redirect/Redirect';
 import { InstructorCoursesPage } from '../instructor/InstructorCoursesPage';
 import { CourseCreationPage } from '../course-creation/CourseCreationPage';
 
-// NOTE: The signed in front page is quite different from landing page so for that it would be good to create a separate page component using some of the same sections as in the landing page.
+// NOTE: I have not yet implemented and probably will not implement a customized landing / homepage for a signed in user to stay within a reasonable scope for this project.
 
 export const SignedInRoutes = () => {
   return (
     <Routes>
+      {/** Default to landing page */}
       <Route path="/" element={<LandingPage />} />
+
+      {/** Instructor routes */}
       <Route path="/instructor/courses" element={<InstructorCoursesPage />} />
       <Route
         path="/instructor"
         element={<Redirect to="/instructor/courses" />}
       />
+
+      {/** Course creation routes */}
       <Route path="/course/create/:step" element={<CourseCreationPage />} />
       <Route
         path="/course/create"
         element={<Redirect to="/course/create/1" />}
       />
+
+      {/** Redirect routes leading to default route */}
       <Route path="/teaching" element={<Redirect to="/instructor" />} />
       <Route path="/signup" element={<Redirect to="/" />} />
+
+      {/** Not found route */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
