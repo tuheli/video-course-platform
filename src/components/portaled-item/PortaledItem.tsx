ab623f9f59d8d7a75e6a8be6cf1a5f9265f7b9ea
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-27 13:51:36 +0300

    Increased z index, moved positioning functions to their own file

--- src/components/portaled-item/PortaledItem.tsx
+++ src/components/portaled-item/PortaledItem.tsx
@@ -1,17 +1,18 @@
 import { Box } from '@mui/material';
 import { useLayoutEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
-
-interface SpaceAround {
-  spaceLeft: number;
-  spaceRight: number;
-  spaceAbove: number;
-  spaceBelow: number;
-}
-
-export type RenderPosition = 'left' | 'right' | 'below';
-
-export type AnchorPoint = 'bottom-left' | 'top-right' | 'bottom-right-end';
+import {
+  getSpaceAroundElement,
+  getBottomLeftPosition,
+  getTopRightPosition,
+  getBottomRightEndPosition,
+  getFittingPosition,
+  getCenteredLeftPosition,
+  getCenteredRightPosition,
+  getCenteredBottomPosition,
+} from './positioningUtilities';
+import { Placement } from './positioningUtilities';
+import { RenderPosition, AnchorPoint } from './positioningUtilities';
 
 interface PortaledItemProps {
   anchorElement: HTMLElement | null;
@@ -20,145 +21,6 @@ interface PortaledItemProps {
   children?: React.ReactNode;
   customOffset?: { top: number; left: number };
 }
-
-interface Placement {
-  top: number;
-  left: number;
-}
-
-interface ReactangleSize {
-  width: number;
-  height: number;
-}
-
-const getFittingPosition = (
-  portalSize: { width: number; height: number },
-  parentSpaceAround: SpaceAround,
-  errorMargin = 20
-): RenderPosition => {
-  const fitsRight =
-    parentSpaceAround.spaceRight - errorMargin >= portalSize.width;
-  const fitsLeft =
-    parentSpaceAround.spaceLeft + errorMargin >= portalSize.width;
-
-  if (fitsRight) return 'right';
-  if (fitsLeft) return 'left';
-  return 'below';
-};
-
-const getSpaceAroundElement = (element: HTMLElement): SpaceAround => {
-  const rect = element.getBoundingClientRect();
-  return {
-    spaceLeft: rect.left,
-    spaceRight: window.innerWidth - rect.right,
-    spaceAbove: rect.top,
-    spaceBelow: window.innerHeight - rect.bottom,
-  };
-};
-
-const getFlexDirectionForDirectionTriangle = (
-  renderPosition: RenderPosition
-) => {
-  switch (renderPosition) {
-    case 'right':
-      return 'row';
-    case 'left':
-      return 'row';
-    case 'below':
-      return 'column';
-    default:
-      return undefined;
-  }
-};
-
-const getTopRightPosition = (anchorElement: HTMLElement) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.top + window.scrollY - 1,
-    left: anchor.right + window.scrollX,
-  };
-
-  return position;
-};
-
-const getBottomLeftPosition = (anchorElement: HTMLElement) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.bottom + window.scrollY,
-    left: anchor.left + window.scrollX,
-  };
-
-  return position;
-};
-
-const getBottomRightPosition = (anchorElement: HTMLElement) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.bottom + window.scrollY,
-    left: anchor.right + window.scrollX,
-  };
-
-  return position;
-};
-
-const getBottomRightEndPosition = (
-  anchorElement: HTMLElement,
-  myWidth: number
-) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.bottom + window.scrollY,
-    left: anchor.right + window.scrollX - myWidth,
-  };
-
-  return position;
-};
-
-const getCenteredRightPosition = (
-  anchorElement: HTMLElement,
-  mySize: ReactangleSize
-) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.top + window.scrollY + (anchor.height - mySize.height) / 2,
-    left: anchor.right + window.scrollX,
-  };
-
-  return position;
-};
-
-const getCenteredLeftPosition = (
-  anchorElement: HTMLElement,
-  mySize: ReactangleSize
-) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.top + window.scrollY + (anchor.height - mySize.height) / 2,
-    left: anchor.left + window.scrollX - mySize.width,
-  };
-
-  return position;
-};
-
-const getCenteredBottomPosition = (
-  anchorElement: HTMLElement,
-  mySize: ReactangleSize
-) => {
-  const anchor = anchorElement.getBoundingClientRect();
-
-  const position = {
-    top: anchor.bottom + window.scrollY,
-    left: anchor.left + window.scrollX + (anchor.width - mySize.width) / 2,
-  };
-
-  return position;
-};
 
 export const PortaledItem = ({
   anchorElement,
@@ -169,6 +31,7 @@ export const PortaledItem = ({
 }: PortaledItemProps) => {
   const [position, setPosition] = useState<Placement | null>(null);
   const ref = useRef<HTMLDivElement | null>(null);
+  const zIndex = 999999;
 
   useLayoutEffect(() => {
     const calculatePosition = () => {
@@ -233,7 +96,7 @@ export const PortaledItem = ({
       sx={{
         display: 'flex',
         position: 'absolute',
-        zIndex: 2,
+        zIndex,
         top: position && position?.top && position.top + customOffsetTop,
         left: position && position?.left && position.left + customOffsetLeft,
       }}
