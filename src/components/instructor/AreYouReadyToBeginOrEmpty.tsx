8b14684f139561d59f692d3ef8a3c3d314b38465
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-27 12:57:39 +0300

    Create AreYouReadyToBeginOrEmpty.tsx

---
+++ src/components/instructor/AreYouReadyToBeginOrEmpty.tsx
@@ -0,0 +1,26 @@
+import { Box } from '@mui/material';
+import { useAppSelector } from '../../app/hooks';
+import { AreYouReadyToBegin } from './AreYouReadyToBegin';
+
+export const AreYouReadyToBeginOrEmpty = () => {
+  const myEmail = useAppSelector((state) => state.me.user?.credentials.email);
+
+  const isAreYouReadyToBeginVisible =
+    useAppSelector((state) => state.courseDrafts).filter(
+      ({ creatorEmail }) => myEmail === creatorEmail
+    ).length === 0;
+
+  return (
+    <>
+      {isAreYouReadyToBeginVisible && (
+        <Box
+          sx={{
+            pb: 8,
+          }}
+        >
+          <AreYouReadyToBegin />
+        </Box>
+      )}
+    </>
+  );
+};
