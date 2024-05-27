3a12fe9ae28f900b7ad1271bd5c246f9ac3ed7ab
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-27 14:03:54 +0300

    Create MyCoursesOrJumpIntoCourseCreation.tsx

---
+++ src/components/instructor/MyCoursesOrJumpIntoCourseCreation.tsx
@@ -0,0 +1,19 @@
+import { useAppSelector } from '../../app/hooks';
+import { JumpIntoCourseCreation } from '../course-creation/JumpIntoCourseCreation';
+import { MyCourses } from './MyCourses';
+
+export const MyCoursesOrJumpIntoCourseCreation = () => {
+  const myEmail = useAppSelector((state) => state.me.user?.credentials.email);
+
+  const isMyCoursesVisible =
+    useAppSelector((state) => state.courseDrafts).filter(
+      ({ creatorEmail }) => myEmail === creatorEmail
+    ).length > 0;
+
+  return (
+    <>
+      {isMyCoursesVisible && <MyCourses />}
+      {!isMyCoursesVisible && <JumpIntoCourseCreation />}
+    </>
+  );
+};
