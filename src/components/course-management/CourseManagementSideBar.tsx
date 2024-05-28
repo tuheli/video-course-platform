70efd4915d5222c1df0eabeee198a828b09a8175
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-28 06:44:26 +0300

    Create CourseManagementSideBar.tsx

---
+++ src/components/course-management/CourseManagementSideBar.tsx
@@ -0,0 +1,14 @@
+import { Stack } from '@mui/material';
+
+export const CourseManagementSideBar = () => {
+  return (
+    <Stack
+      sx={{
+        flexDirection: 'column',
+        bgcolor: 'yellow',
+        width: 280,
+        height: 600,
+      }}
+    ></Stack>
+  );
+};
