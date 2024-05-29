834043664a3db2643a7422b2bc528f2489817623
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-29 19:09:00 +0300

    Added comment

--- src/components/border-animation-wrapper/BorderAnimationWrapper.tsx
+++ src/components/border-animation-wrapper/BorderAnimationWrapper.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from 'react';
 
+// NOTE: The initial reason for creating
+// this component was  was to give visual feedback
+// about the new placement of drag and drop
+// items
+
 interface BorderAnimationWrapperProps {
   children: ReactNode;
 }
