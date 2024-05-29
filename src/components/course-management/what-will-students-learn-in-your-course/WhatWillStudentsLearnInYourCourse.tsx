fca250dc093bb2fa3c49ab85ae70fa4c2f157c9b
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-29 10:56:14 +0300

    Working on drag and drop

--- src/components/course-management/what-will-students-learn-in-your-course/WhatWillStudentsLearnInYourCourse.tsx
+++ src/components/course-management/what-will-students-learn-in-your-course/WhatWillStudentsLearnInYourCourse.tsx
@@ -3,12 +3,71 @@ import { LightColoredRouterLink } from '../LightColoredRouterLink';
 import { EditLearningObjectiveItem } from './EditLearningObjectiveItem';
 import { AddLearningObjectiveButton } from './AddLearningObjectiveButton';
 import { useCourseDraft } from '../../../hooks/useCourseDraft';
+import { DroppableArea } from '../../drag-and-drop/DroppableArea';
+import { Draggable, IDraggable } from '../../drag-and-drop/Draggable';
+import { DragAndDropContext } from '../../../contexts/DragAndDropContext';
+import { useEffect, useState } from 'react';
+import { TextWithId } from '../../../features/courseDraftsSlice';
 
 export const WhatWillStudentsLearnInYourCourse = () => {
+  const [learningObjectives, setLearningObjectives] = useState<TextWithId[]>(
+    []
+  );
+
   const courseDraft = useCourseDraft();
 
-  const learningObjectives =
-    courseDraft?.courseContent.intendedLearnersSection.learningObjectives;
+  useEffect(() => {
+    const learningObjectives =
+      courseDraft?.courseContent.intendedLearnersSection.learningObjectives;
+
+    if (!learningObjectives) return;
+
+    setLearningObjectives(learningObjectives);
+  }, []);
+
+  const changeOrder = (draggables: IDraggable[]) => {
+    const stateWithYPositions = learningObjectives.map((learningObjective) => {
+      // Find inside map is slow in theory but the item counts are tiny
+      const draggable = draggables.find(
+        (draggable) => draggable.id === learningObjective.id
+      );
+
+      if (!draggable) {
+        // This would essentially break the ordering
+        // by setting the item on top but it would
+        // be a bug to not have the draggable
+        // found. Also checking if the id mapping exists
+        // from draggables to learning objectives
+        // would not help because if a mapping does not exist
+        // reordering just could not be done correctly and
+        // an early return would cause nothing to happen
+        return {
+          ...learningObjective,
+          yPosition: 0,
+        };
+      }
+
+      return {
+        ...learningObjective,
+        yPosition: draggable.yPosition,
+      };
+    });
+
+    const stateReordered = stateWithYPositions.sort(
+      (a, b) => a.yPosition - b.yPosition
+    );
+
+    const reorderedStateWithoutYPositions: TextWithId[] = stateReordered.map(
+      ({ id, text }) => {
+        return {
+          id,
+          text,
+        };
+      }
+    );
+
+    setLearningObjectives(reorderedStateWithoutYPositions);
+  };
 
   const courseDraftId = courseDraft?.id;
 
@@ -35,13 +94,18 @@ export const WhatWillStudentsLearnInYourCourse = () => {
         </LightColoredRouterLink>{' '}
         that learners can expect to achieve after completing your course.
       </Typography>
-      {learningObjectives.map((learningObjective) => (
-        <EditLearningObjectiveItem
-          key={learningObjective.id}
-          courseDraft={courseDraft}
-          learningObjective={learningObjective}
-        />
-      ))}
+      <DragAndDropContext.Provider value={{ changeOrder }}>
+        <DroppableArea>
+          {learningObjectives.map((learningObjective) => (
+            <Draggable id={learningObjective.id} key={learningObjective.id}>
+              <EditLearningObjectiveItem
+                courseDraft={courseDraft}
+                learningObjective={learningObjective}
+              />
+            </Draggable>
+          ))}
+        </DroppableArea>
+      </DragAndDropContext.Provider>
       <AddLearningObjectiveButton />
     </Stack>
   );
