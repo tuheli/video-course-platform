2ec71b3af73caf56058ed82fd167cfacb3e1fc65
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-29 19:15:38 +0300

    Updated comment

--- src/components/border-animation-wrapper/BorderAnimationWrapper.tsx
+++ src/components/border-animation-wrapper/BorderAnimationWrapper.tsx
@@ -5,6 +5,14 @@ import { ReactNode } from 'react';
 // about the new placement of drag and drop
 // items
 
+// The animation timings are hard coded in the css
+// file. In order for the borders to animate
+// nicely with any rectangle the values need
+// to be calulated using the elements size.
+// Current durations are set for input fields
+// used in course management page.
+// If needed create a hook for dynamic durations.
+
 interface BorderAnimationWrapperProps {
   children: ReactNode;
 }
