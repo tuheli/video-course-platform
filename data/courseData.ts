5bc216fd49fed9d4936bd98cee405f202112f4b9
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-29 14:15:01 +0300

    Exporting function

--- data/courseData.ts
+++ data/courseData.ts
@@ -40,7 +40,7 @@ interface CourseSubcategory {
   topics: CourseTopic[];
 }
 
-const getRandomInt = (min: number, max: number) => {
+export const getRandomInt = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
