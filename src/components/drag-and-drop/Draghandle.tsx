c8b84fb09e43c2c0862d61977ec31bc3520b4f3c
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-29 10:54:37 +0300

    Create Draghandle.tsx

---
+++ src/components/drag-and-drop/Draghandle.tsx
@@ -0,0 +1,42 @@
+import { Box } from '@mui/material';
+import { useDraggableContext } from '../../hooks/useDraggableContext';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const Draghandle = () => {
+  const { setIsDraggable } = useDraggableContext();
+
+  const onMouseEnter = () => {
+    setIsDraggable(true);
+  };
+
+  const onMouseLeave = () => {
+    setIsDraggable(false);
+  };
+
+  return (
+    <div
+      className="draghandle"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      style={{
+        cursor: 'move',
+      }}
+    >
+      <Box
+        sx={{
+          width: 54,
+          height: 54,
+          bgcolor: 'orange',
+          border: '1px solid',
+          borderColor: 'text.primary',
+          borderLeft: 'none',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <MenuIcon />
+      </Box>
+    </div>
+  );
+};
