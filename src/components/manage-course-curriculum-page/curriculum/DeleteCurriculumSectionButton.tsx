517d658e51a9e86e06cbd62be6eb1a4cdccb4247
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-30 15:00:11 +0300

    Create DeleteCurriculumSectionButton.tsx

---
+++ src/components/manage-course-curriculum-page/curriculum/DeleteCurriculumSectionButton.tsx
@@ -0,0 +1,41 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box } from '@mui/material';
+import { useAppDispatch } from '../../../app/hooks';
+import { deletedCurriculumSection } from '../../../features/courseDraftsSlice';
+
+interface DeleteCurriculumSectionButtonProps {
+  courseDraftId: string;
+  curriculumSectionId: string;
+}
+
+export const DeleteCurriculumSectionButton = ({
+  courseDraftId,
+  curriculumSectionId,
+}: DeleteCurriculumSectionButtonProps) => {
+  const dispatch = useAppDispatch();
+  const cursor = 'pointer';
+
+  const onClick = () => {
+    dispatch(deletedCurriculumSection({ courseDraftId, curriculumSectionId }));
+  };
+
+  return (
+    <Box
+      onClick={onClick}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        '&:hover': {
+          cursor,
+        },
+      }}
+    >
+      <DeleteIcon
+        sx={{
+          fontSize: 17,
+        }}
+      />
+    </Box>
+  );
+};
