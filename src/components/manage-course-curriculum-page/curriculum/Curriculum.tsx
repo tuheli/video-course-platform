3fe5c56fc48ef2119be5f84ef4fd6b7501fdf250
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-30 15:00:24 +0300

    Create Curriculum.tsx

---
+++ src/components/manage-course-curriculum-page/curriculum/Curriculum.tsx
@@ -0,0 +1,29 @@
+import { Stack } from '@mui/material';
+import { useCurriculum } from '../../../hooks/useCurriculum';
+import { CurriculumSection } from './CurriculumSection';
+import { AddCurriculumSectionButton } from './AddCurriculumSectionButton';
+
+export const Curriculum = () => {
+  const { curriculum, courseDraft } = useCurriculum();
+
+  if (!courseDraft) return null;
+
+  return (
+    <Stack
+      sx={{
+        flexDirection: 'column',
+        gap: 4,
+      }}
+    >
+      {curriculum.map((curriculumSection, index) => (
+        <CurriculumSection
+          key={curriculumSection.id}
+          curriculumSection={curriculumSection}
+          courseDraftId={courseDraft.id}
+          index={index}
+        />
+      ))}
+      <AddCurriculumSectionButton />
+    </Stack>
+  );
+};
