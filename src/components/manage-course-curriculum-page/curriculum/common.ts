57702c8674e0c3752a043491476eda3adafdc103
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-31 14:59:55 +0300

    Update common.ts

--- src/components/manage-course-curriculum-page/curriculum/common.ts
+++ src/components/manage-course-curriculum-page/curriculum/common.ts
@@ -1,1 +1,13 @@
-export const animationDurationSeconds = 1;
+import { SxProps } from '@mui/material';
+
+export const animationDurationSeconds = 0.5;
+
+export const inputOuterDivSx: SxProps = {
+  height: 28,
+  pl: 1,
+  mb: 1,
+};
+
+export const inputSx: SxProps = {
+  fontSize: 14,
+};
