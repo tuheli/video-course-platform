8fe26053cc5c9b508f4d6c2605214ef73b54f2fd
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-31 10:14:07 +0300

    Create AddCurriculumItemButton.tsx

---
+++ src/components/manage-course-curriculum-page/curriculum/AddCurriculumItemButton.tsx
@@ -0,0 +1,57 @@
+import { useEnableActionTimer } from '../../../hooks/useEnableActionTimer';
+import { AddMoreButton } from '../../manage-course-goals-page/AddMoreButton';
+import { animationDurationSeconds } from './common';
+
+interface AddCurriculumItemButtonProps {
+  setOptionsVisibility: (isVisible: boolean) => void;
+  animateIconBack: boolean;
+}
+
+export const AddCurriculumItemButton = ({
+  animateIconBack,
+  setOptionsVisibility,
+}: AddCurriculumItemButtonProps) => {
+  const { isEnabled } = useEnableActionTimer(animationDurationSeconds * 1000);
+
+  const isClickAllowed = animateIconBack ? isEnabled : true;
+
+  const onClick = () => {
+    if (!isClickAllowed) return;
+    setOptionsVisibility(true);
+  };
+
+  return (
+    <AddMoreButton
+      addIconContainerSx={
+        animateIconBack
+          ? {
+              animation: `closeIconMoveAnimation ${animationDurationSeconds}s ease-in forwards`,
+              animationDirection: 'reverse',
+            }
+          : {}
+      }
+      addIconSx={
+        animateIconBack
+          ? {
+              // transform: 'rotate(45deg)',
+              animation: `closeIconRollAnimation ${animationDurationSeconds}s ease-in forwards`,
+              animationDirection: 'reverse',
+            }
+          : {}
+      }
+      text="Curriculum item"
+      onClick={onClick}
+      sx={{
+        pl: 1,
+        border: '1px solid',
+        borderColor: 'text.primary',
+        color: 'text.primary',
+        bgcolor: 'background.default',
+        '&:hover': {
+          bgcolor: 'background.paperDarker',
+          cursor: 'pointer',
+        },
+      }}
+    />
+  );
+};
