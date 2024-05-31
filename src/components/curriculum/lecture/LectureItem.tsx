a8ca5a1fb95a3986aed3bf7138e8e9c864879433
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-31 17:58:43 +0300

    Create LectureItem.tsx

---
+++ src/components/curriculum/lecture/LectureItem.tsx
@@ -0,0 +1,65 @@
+import { ChangeEvent } from 'react';
+import { useAppDispatch } from '../../../app/hooks';
+import {
+  Lesson,
+  updatedLectureTitle,
+} from '../../../features/courseDraftsSlice';
+import { useEditableCurriculumItem } from '../../../hooks/useEditableCurriculumItem';
+import { EditCurriculumItemLayout } from '../EditCurriculumItemLayout';
+import { SectionItemHeadingLayout } from '../SectionItemHeadingLayout';
+import { useCurriculumSectionContext } from '../../../hooks/useCurriculumSectionContext';
+
+interface LectureItemProps {
+  lecture: Lesson;
+  index: number;
+}
+
+export const LectureItem = ({ lecture, index }: LectureItemProps) => {
+  const { isHeadingPartVisible, changeHeadingVisibility } =
+    useEditableCurriculumItem();
+  const { courseDraftId, curriculumSection } = useCurriculumSectionContext();
+
+  const dispatch = useAppDispatch();
+
+  const onChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      updatedLectureTitle({
+        courseDraftId,
+        curriculumSectionId: curriculumSection.id,
+        lectureId: lecture.id,
+        newLectureTitle: event.target.value,
+      })
+    );
+  };
+
+  const onClickCancel = () => {
+    changeHeadingVisibility(true);
+  };
+
+  const onClickSave = () => {
+    changeHeadingVisibility(true);
+  };
+
+  return (
+    <>
+      {isHeadingPartVisible && (
+        <SectionItemHeadingLayout
+          itemName={'Lecture'}
+          index={index}
+          title={lecture.name}
+          changeHeadingVisibility={changeHeadingVisibility}
+        />
+      )}
+      {!isHeadingPartVisible && (
+        <EditCurriculumItemLayout
+          title={`Lecture ${index + 1}:`}
+          titleValue={lecture.name}
+          saveButtonText="Save Lecture"
+          onChangeTitle={onChangeTitle}
+          onClickCancel={onClickCancel}
+          onClickSave={onClickSave}
+        />
+      )}
+    </>
+  );
+};
