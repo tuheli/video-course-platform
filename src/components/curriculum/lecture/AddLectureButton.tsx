2c626da6f8a35c6b5ed41146f9e3daad919082fc
tuheli <94482031+tuheli@users.noreply.github.com>
2024-05-31 18:19:40 +0300

    Added props for component reusability

--- src/components/curriculum/lecture/AddLectureButton.tsx
+++ src/components/curriculum/lecture/AddLectureButton.tsx
@@ -1,16 +1,31 @@
+import { EditableItemType } from '../../../contexts/CurriculumSectionContext';
 import { useCurriculumSectionContext } from '../../../hooks/useCurriculumSectionContext';
 import { AddMoreButton } from '../../manage-course-goals-page/AddMoreButton';
 
-export const AddLectureButton = () => {
+// NOTE: I added undefined as a type option
+// to show a few extra buttons which
+// dont do anything. They also will not
+// cause a re-render at this point since
+// the state doesn't change.
+
+interface AddableItemOptionButtonProps {
+  type: EditableItemType | undefined;
+  text: string;
+}
+
+export const AddableItemOptionButton = ({
+  type,
+  text,
+}: AddableItemOptionButtonProps) => {
   const { setEditingItemType } = useCurriculumSectionContext();
 
   const onClick = () => {
-    setEditingItemType('addLecture');
+    setEditingItemType(type);
   };
 
   return (
     <AddMoreButton
-      text="Lecture"
+      text={text}
       onClick={onClick}
       sx={{
         pl: 1,
