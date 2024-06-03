ec8941ce9f19a75993a20bf56d9df42a37b4c41e
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-03 16:58:31 +0300

    State starts from empty descendant

--- src/components/text-editor/TextEditor.tsx
+++ src/components/text-editor/TextEditor.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import isHotkey from 'is-hotkey';
 import {
   Editable,
@@ -16,94 +16,102 @@ import { Element } from './Element';
 import { Leaf } from './Leaf';
 import { BlockButton } from './buttons/BlockButton';
 import { MarkButton } from './buttons/MarkButton';
+import { Divider, Stack } from '@mui/material';
+import { TextEditorContext } from '../../contexts/TextEditorContext';
+
+// TODO: Ensure the editor gets focused,
+// placeholder node is removed and editor
+// contains atleast an empty text element on focus
+// or if mark button is pressed
+
+// TODO: Remove list formats if there is no
+// text in the editor and user presses backspace
+
+// TODO: Style the list formats so they dont
+// add margins and paddings
 
 const initialValue: Descendant[] = [
   {
     type: 'paragraph',
-    children: [
-      { text: 'This is editable ' },
-      { text: 'rich', bold: true },
-      { text: ' text, ' },
-      { text: 'much', italic: true },
-      { text: ' better than a ' },
-      { text: '<textarea>', code: true },
-      { text: '!' },
-    ],
-  },
-  {
-    type: 'paragraph',
-    children: [
-      {
-        text: "Since it's rich text, you can do things like turn a selection of text ",
-      },
-      { text: 'bold', bold: true },
-      {
-        text: ', or add a semantically rendered block quote in the middle of the page, like this:',
-      },
-    ],
-  },
-  {
-    type: 'block-quote',
-    children: [{ text: 'A wise quote.' }],
-  },
-  {
-    type: 'paragraph',
-    align: 'center',
-    children: [{ text: 'Try it out for yourself!' }],
+    children: [{ text: '' }],
   },
 ];
 
-export const TextEditor = () => {
+interface TextEditorProps {
+  placeholder: string;
+}
+
+export const TextEditor = ({ placeholder }: TextEditorProps) => {
+  const [isPlaceholderVisible, setIsPlaceholderVisible] = useState(true);
+
   const renderElement = useCallback(
     (props: RenderElementProps) => <Element {...props} />,
     []
   );
+
   const renderLeaf = useCallback(
     (props: RenderLeafProps) => <Leaf {...props} />,
     []
   );
+
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
   return (
     <Slate editor={editor} initialValue={initialValue}>
-      <Toolbar>
-        <MarkButton format="bold" iconName="format_bold" />
-        <MarkButton format="italic" iconName="format_italic" />
-        <MarkButton format="underline" iconName="format_underlined" />
-        <MarkButton format="code" iconName="code" />
-        <BlockButton blockPropertyName="heading-one" icon="looks_one" />
-        <BlockButton blockPropertyName="heading-two" icon="looks_two" />
-        <BlockButton blockPropertyName="block-quote" icon="format_quote" />
-        <BlockButton
-          blockPropertyName="numbered-list"
-          icon="format_list_numbered"
-        />
-        <BlockButton
-          blockPropertyName="bulleted-list"
-          icon="format_list_bulleted"
-        />
-        <BlockButton align="left" icon="format_align_left" />
-        <BlockButton align="center" icon="format_align_center" />
-        <BlockButton align="right" icon="format_align_right" />
-        <BlockButton align="justify" icon="format_align_justify" />
-      </Toolbar>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        placeholder="Enter some rich text…"
-        spellCheck
-        autoFocus
-        onKeyDown={(event) => {
-          for (const hotkey in HOTKEYS) {
-            if (isHotkey(hotkey, event as any)) {
-              event.preventDefault();
-              if (!isKnownHotkey(hotkey)) return;
-              const mark = HOTKEYS[hotkey];
-              toggleMark(editor, mark);
-            }
-          }
+      <TextEditorContext.Provider
+        value={{
+          setIsPlaceholderVisible,
         }}
-      />
+      >
+        <Stack
+          sx={{
+            border: '1px solid',
+            borderColor: 'text.primary',
+          }}
+        >
+          <Toolbar>
+            <MarkButton format="bold" iconName="format_bold" />
+            <MarkButton format="italic" iconName="format_italic" />
+            <BlockButton
+              blockPropertyName="numbered-list"
+              icon="format_list_numbered"
+            />
+            <BlockButton
+              blockPropertyName="bulleted-list"
+              icon="format_list_bulleted"
+            />
+          </Toolbar>
+          <Divider
+            sx={{
+              borderColor: 'text.primary',
+              mb: 1,
+            }}
+          />
+          <Editable
+            renderElement={renderElement}
+            renderLeaf={renderLeaf}
+            placeholder={isPlaceholderVisible ? placeholder : ''}
+            spellCheck={false}
+            autoFocus={false}
+            onKeyDown={(event) => {
+              for (const hotkey in HOTKEYS) {
+                if (isHotkey(hotkey, event as any)) {
+                  event.preventDefault();
+                  if (!isKnownHotkey(hotkey)) return;
+                  const mark = HOTKEYS[hotkey];
+                  toggleMark(editor, mark);
+                }
+              }
+            }}
+            style={{
+              outline: 'none',
+              paddingLeft: 14,
+              paddingRight: 14,
+              paddingBottom: 32,
+            }}
+          />
+        </Stack>
+      </TextEditorContext.Provider>
     </Slate>
   );
 };
