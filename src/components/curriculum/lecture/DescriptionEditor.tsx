108151ada6c2878681dadeb5768e727a023af03a
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-04 08:13:26 +0300

    Lecture description is saved in redux

--- src/components/curriculum/lecture/DescriptionEditor.tsx
+++ src/components/curriculum/lecture/DescriptionEditor.tsx
@@ -1,10 +1,31 @@
 import { Stack, Typography } from '@mui/material';
 import { TextEditor } from '../../text-editor/TextEditor';
+import { Descendant } from 'slate';
+import { useLectureContext } from '../../../hooks/useLectureContext';
+import { useCurriculumSectionContext } from '../../../hooks/useCurriculumSectionContext';
+import { useAppDispatch } from '../../../app/hooks';
+import { updatedLecture } from '../../../features/courseDraftsSlice';
 
 const placeholder =
   'Add a description. Include what students will be able to do after completing the lecture.';
 
 export const DesctiptionEditor = () => {
+  const { lecture } = useLectureContext();
+  const { courseDraftId, curriculumSection } = useCurriculumSectionContext();
+  const dispatch = useAppDispatch();
+
+  const onChange = (value: Descendant[]) => {
+    dispatch(
+      updatedLecture({
+        courseDraftId,
+        curriculumSectionId: curriculumSection.id,
+        lectureId: lecture.id,
+        newValue: JSON.stringify(value),
+        propertyName: 'description',
+      })
+    );
+  };
+
   return (
     <Stack
       sx={{
@@ -18,7 +39,7 @@ export const DesctiptionEditor = () => {
       >
         Lecture Description
       </Typography>
-      <TextEditor placeholder={placeholder} />
+      <TextEditor placeholder={placeholder} onChange={onChange} />
     </Stack>
   );
 };
