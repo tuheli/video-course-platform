72d05ad72defcbd1840f0557b7916ce6e60b061f
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-04 10:41:55 +0300

    Passing coursedraft id as a prop

--- src/components/curriculum/section/AddSectionButton.tsx
+++ src/components/curriculum/section/AddSectionButton.tsx
@@ -1,15 +1,16 @@
 import { useAppDispatch } from '../../../app/hooks';
 import { addedCurriculumSection } from '../../../features/courseDraftsSlice';
-import { useCourseDraft } from '../../../hooks/useCourseDraft';
 import { AddMoreButtonDarkVariant } from '../../manage-course-goals-page/AddMoreButtonDarkVariant';
 
-export const AddSectionButton = () => {
-  const courseDraft = useCourseDraft();
+interface AddSectionButtonProps {
+  courseDraftId: string;
+}
+
+export const AddSectionButton = ({ courseDraftId }: AddSectionButtonProps) => {
   const dispatch = useAppDispatch();
 
   const onClick = () => {
-    if (!courseDraft) return;
-    dispatch(addedCurriculumSection({ courseDraftId: courseDraft.id }));
+    dispatch(addedCurriculumSection({ courseDraftId }));
   };
 
   return <AddMoreButtonDarkVariant text="Section" onClick={onClick} />;
