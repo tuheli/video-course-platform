1178a416c8ab437c93a6c2b8f0559724916af7ca
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-04 15:32:59 +0300

    Added more props to context state
    - The info for if something is dragged is needed for the (kind of complex) animation state in curriculum item addition button
    - That is the rolling add icon
    - During a drag the animation is forcefully disabled to disable playing the animation on reorder

--- src/contexts/DragAndDropContext.tsx
+++ src/contexts/DragAndDropContext.tsx
@@ -2,10 +2,14 @@ import { createContext } from 'react';
 
 interface DragAndDropContextState {
   itemsState: Array<{ id: string }>;
+  isSomethingDragged?: boolean;
   changeOrder: (newOrder: Array<{ id: string; orderIndex: number }>) => void;
+  setIsSomethingDragged?: (isSomethingDragged: boolean) => void;
 }
 
 export const DragAndDropContext = createContext<DragAndDropContextState>({
   itemsState: [],
+  isSomethingDragged: false,
   changeOrder: () => {},
+  setIsSomethingDragged: () => {},
 });
