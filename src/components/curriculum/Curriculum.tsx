707f0d7c3223e1f6d79c63a64872f40970932b21
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-04 15:37:44 +0300

    Curriculum sections can be reordered by dragging

--- src/components/curriculum/Curriculum.tsx
+++ src/components/curriculum/Curriculum.tsx
@@ -3,6 +3,13 @@ import { Section } from './section/Section';
 import { useCurriculumFromParams } from '../../hooks/useCurriculum';
 import { AddSectionButton } from './section/AddSectionButton';
 import { cleanupCurriculumLocalStorage } from './utils';
+import { DroppableArea } from '../drag-and-drop/DroppableArea';
+import { Draggable } from '../drag-and-drop/Draggable';
+import { DragAndDropContext } from '../../contexts/DragAndDropContext';
+import { ItemWithOrderIndex } from '../drag-and-drop/utils';
+import { reorderedSections } from '../../features/courseDraftsSlice';
+import { useAppDispatch } from '../../app/hooks';
+import { useState } from 'react';
 
 interface CurriculumProps {
   forcedCourseId?: string;
@@ -10,27 +17,48 @@ interface CurriculumProps {
 
 export const Curriculum = ({ forcedCourseId }: CurriculumProps) => {
   const { curriculum, courseDraft } = useCurriculumFromParams(forcedCourseId);
+  const [isSomethingDragged, setIsSomethingDragged] = useState(false);
+  const dispatch = useAppDispatch();
 
   if (!courseDraft) return null;
 
   cleanupCurriculumLocalStorage(courseDraft.id, curriculum);
 
+  const changeSectionsOrder = (newOrder: ItemWithOrderIndex[]) => {
+    if (!courseDraft) return;
+    dispatch(reorderedSections({ courseDraftId: courseDraft.id, newOrder }));
+  };
+
   return (
-    <Stack
-      sx={{
-        flexDirection: 'column',
-        gap: 4,
-      }}
-    >
-      {curriculum.map((curriculumSection, index) => (
-        <Section
-          key={curriculumSection.id}
-          curriculumSection={curriculumSection}
-          courseDraftId={courseDraft.id}
-          index={index}
-        />
-      ))}
+    <>
+      <DragAndDropContext.Provider
+        value={{
+          itemsState: curriculum,
+          isSomethingDragged,
+          changeOrder: changeSectionsOrder,
+          setIsSomethingDragged,
+        }}
+      >
+        <DroppableArea>
+          <Stack
+            sx={{
+              flexDirection: 'column',
+              gap: 4,
+            }}
+          >
+            {curriculum.map((curriculumSection, index) => (
+              <Draggable key={curriculumSection.id} id={curriculumSection.id}>
+                <Section
+                  curriculumSection={curriculumSection}
+                  courseDraftId={courseDraft.id}
+                  index={index}
+                />
+              </Draggable>
+            ))}
+          </Stack>
+        </DroppableArea>
+      </DragAndDropContext.Provider>
       <AddSectionButton courseDraftId={courseDraft.id} />
-    </Stack>
+    </>
   );
 };
