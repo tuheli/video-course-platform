0e97433c0828d76add361d4386f7d5c159c8c621
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-04 15:34:53 +0300

    Added hook to disable options component from animating if something is being dragged

--- src/components/curriculum/ItemSelection.tsx
+++ src/components/curriculum/ItemSelection.tsx
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AddItemButton } from './AddItemButton';
 import { ItemOptions } from './ItemOptions';
+import { useDragAndDropContext } from '../../hooks/useDragAndDropContext';
+import { useCurriculumSectionContext } from '../../hooks/useCurriculumSectionContext';
 
 export const ItemSelection = () => {
   const [animateCloseIconBack, setAnimateCloseIconBack] = useState(false);
   const [isSelectionVisible, setIsSelectionVisible] = useState(false);
+  useState(false);
+  const { setIsOptionsAnimationEnabled } = useCurriculumSectionContext();
+  const { isSomethingDragged } = useDragAndDropContext();
 
   const onItemOptionsClose = () => {
     setAnimateCloseIconBack(true);
     setIsSelectionVisible(false);
   };
 
+  useEffect(() => {
+    if (!isSomethingDragged) return;
+    setIsOptionsAnimationEnabled(false);
+  }, [isSomethingDragged]);
+
   return (
     <>
       {!isSelectionVisible && (
         <AddItemButton
-          setOptionsVisibility={setIsSelectionVisible}
           animateIconBack={animateCloseIconBack}
+          setOptionsVisibility={setIsSelectionVisible}
         />
       )}
       {isSelectionVisible && <ItemOptions setVisibility={onItemOptionsClose} />}
