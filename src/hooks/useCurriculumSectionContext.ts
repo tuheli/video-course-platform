4eaaddd7572df1a082e1010fb4f0923a193a8853
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-04 10:38:54 +0300

    Removed unnecessary error throw
    - The value is never undefined

--- src/hooks/useCurriculumSectionContext.ts
+++ src/hooks/useCurriculumSectionContext.ts
@@ -3,12 +3,5 @@ import { CurriculumSectionContext } from '../contexts/CurriculumSectionContext';
 
 export const useCurriculumSectionContext = () => {
   const context = useContext(CurriculumSectionContext);
-
-  if (!context) {
-    throw new Error(
-      'useCurriculumSectionContext must be used within a CurriculumSectionProvider'
-    );
-  }
-
   return context;
 };
--- src/hooks/useLectureContext.ts
+++ src/hooks/useLectureContext.ts
@@ -3,12 +3,5 @@ import { LectureContext } from '../contexts/LectureContext';
 
 export const useLectureContext = () => {
   const context = useContext(LectureContext);
-
-  if (context === undefined) {
-    throw new Error(
-      'useLectureContext must be used within a LectureContext provider'
-    );
-  }
-
   return context;
 };
