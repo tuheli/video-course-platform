1c99b412453ec24be8d85b219e526c6c6240ffc3
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-05 14:49:33 +0300

    Update useEditableCurriculumItem.ts

--- src/hooks/useEditableCurriculumItem.ts
+++ src/hooks/useEditableCurriculumItem.ts
@@ -4,15 +4,18 @@ export const useEditableCurriculumItem = () => {
   const [isHeadingVisible, setIsHeadingPartVisible] = useState(true);
   const [isDeleteButtonVisible, setIsDeleteButtonVisible] = useState(false);
   const [isEditButtonVisible, setIsEditSectionButtonVisible] = useState(false);
+  const [areChildrenVisible, setAreChildrenVisible] = useState(false);
 
   const onMouseEnter = () => {
     setIsDeleteButtonVisible(true);
     setIsEditSectionButtonVisible(true);
+    setAreChildrenVisible(true);
   };
 
   const onMouseLeave = () => {
     setIsDeleteButtonVisible(false);
     setIsEditSectionButtonVisible(false);
+    setAreChildrenVisible(false);
   };
 
   const changeHeadingVisibility = (isVisible: boolean) => {
@@ -23,6 +26,7 @@ export const useEditableCurriculumItem = () => {
     isHeadingVisible,
     isDeleteButtonVisible,
     isEditButtonVisible,
+    areChildrenVisible,
     onMouseEnter,
     onMouseLeave,
     changeHeadingVisibility,
