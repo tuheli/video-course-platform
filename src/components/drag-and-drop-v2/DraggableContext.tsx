68bc480af17b16177aefd3322b1c08127378789d
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-10 16:14:49 +0300

    Delete DraggableContext.tsx

--- src/components/drag-and-drop-v2/DraggableContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface DraggableContextProps {
-  setIsDraggable: (isDraggable: boolean) => void;
-}
-
-export const DraggableContext = createContext<DraggableContextProps>({
-  setIsDraggable: () => {},
-});
