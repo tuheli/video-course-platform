02fbe94b6a6e253cbec67ba95304737f9fd01652
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-10 16:14:53 +0300

    Delete useDraggableContext.ts

--- src/components/drag-and-drop-v2/useDraggableContext.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { DraggableContext } from './DraggableContext';
-
-export const useDraggableContext = () => {
-  const context = useContext(DraggableContext);
-  return context;
-};
