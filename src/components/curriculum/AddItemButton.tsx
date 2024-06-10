8e9dee7d7a97c14121b618948f7de26586034401
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-10 11:50:08 +0300

    Removed animation

--- src/components/curriculum/AddItemButton.tsx
+++ src/components/curriculum/AddItemButton.tsx
@@ -1,53 +1,17 @@
 import { useCurriculumSectionContext } from '../../hooks/useCurriculumSectionContext';
-import { useEnableActionTimer } from '../../hooks/useEnableActionTimer';
 import { AddMoreButtonDarkVariant } from '../manage-course-goals-page/AddMoreButtonDarkVariant';
-import { animationDurationSeconds } from './common';
 
 interface AddItemButtonProps {
-  animateIconBack: boolean;
   setOptionsVisibility: (isVisible: boolean) => void;
 }
 
-export const AddItemButton = ({
-  animateIconBack,
-  setOptionsVisibility,
-}: AddItemButtonProps) => {
-  const { isEnabled } = useEnableActionTimer(animationDurationSeconds * 1000);
-  const {
-    setEditingItemType,
-    setIsOptionsAnimationEnabled,
-    isOptionsAnimationEnabled,
-  } = useCurriculumSectionContext();
-
-  const isClickAllowed = animateIconBack ? isEnabled : true;
+export const AddItemButton = ({ setOptionsVisibility }: AddItemButtonProps) => {
+  const { setEditingItemType } = useCurriculumSectionContext();
 
   const onClick = () => {
-    if (!isClickAllowed) return;
     setEditingItemType(undefined);
-    setIsOptionsAnimationEnabled(true);
     setOptionsVisibility(true);
   };
 
-  return (
-    <AddMoreButtonDarkVariant
-      addIconContainerSx={
-        isOptionsAnimationEnabled && animateIconBack
-          ? {
-              animation: `closeIconMoveAnimation ${animationDurationSeconds}s ease-in forwards`,
-              animationDirection: 'reverse',
-            }
-          : {}
-      }
-      addIconSx={
-        isOptionsAnimationEnabled && animateIconBack
-          ? {
-              animation: `closeIconRollAnimation ${animationDurationSeconds}s ease-in forwards`,
-              animationDirection: 'reverse',
-            }
-          : {}
-      }
-      text="Curriculum item"
-      onClick={onClick}
-    />
-  );
+  return <AddMoreButtonDarkVariant text="Curriculum item" onClick={onClick} />;
 };
