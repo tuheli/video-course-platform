30eeb3abe3189f89210da010cfe132f53cb9d40c
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-10 11:56:31 +0300

    Updated background color

--- src/components/sign-up/StyledTextInput.tsx
+++ src/components/sign-up/StyledTextInput.tsx
@@ -108,7 +108,7 @@ export const TextInput = ({
 export const StyledInput = styled('input')(() => ({
   border: 'none',
   outline: 'none',
-  backgroundColor: 'white',
+  backgroundColor: 'inherit',
   width: '100%',
   height: '100%',
 }));
