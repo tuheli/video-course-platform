b16a090f4fca9d0cf70aacb8f9baf3595e1ebd8c
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-11 07:53:41 +0300

    Added backgroundcolor

--- src/components/manage-course-goals-page/EditableTextItem.tsx
+++ src/components/manage-course-goals-page/EditableTextItem.tsx
@@ -70,6 +70,7 @@ export const EditableTextItem = ({
       onMouseLeave={onMouseLeave}
       sx={{
         flexDirection: 'row',
+        bgcolor: 'background.default',
       }}
     >
       <InputFieldWithMaxCharacters
