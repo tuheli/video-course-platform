f11e03d01f5e52331100fc9dc94f9636cd792795
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-11 10:44:35 +0300

    Create CourseDraftPreviewPage.tsx

---
+++ src/components/course-draft-preview/CourseDraftPreviewPage.tsx
@@ -0,0 +1,49 @@
+import { AppBar, Box, Toolbar } from '@mui/material';
+import { useScrollToTop } from '../../hooks/useScrollToTop';
+import { Footer } from '../footer/Footer';
+import { Layout } from './Layout';
+import { CompanyLogo } from '../appbar/CompanyLogo';
+
+// NOTE: App bar is temporarily
+// course management app bar
+
+export const CourseDraftPreviewPage = () => {
+  useScrollToTop();
+
+  return (
+    <Box
+      sx={{
+        flexDirection: 'column',
+        display: 'flex',
+        minHeight: '100vh',
+      }}
+    >
+      <AppBar
+        sx={{
+          position: 'static',
+        }}
+      >
+        <Toolbar
+          sx={{
+            boxShadow: 'none',
+            bgcolor: 'background.dark',
+          }}
+        >
+          <CompanyLogo isWhite={true} />
+        </Toolbar>
+      </AppBar>
+      <Box>
+        <Layout />
+      </Box>
+      <Box
+        sx={{
+          py: 4,
+          bgcolor: 'background.dark',
+          marginTop: 'auto',
+        }}
+      >
+        <Footer />
+      </Box>
+    </Box>
+  );
+};
