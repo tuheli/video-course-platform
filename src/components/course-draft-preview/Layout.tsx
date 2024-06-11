bd35308ca6582a3eb18a43a798eb9f231606c0e6
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-11 17:11:16 +0300

    Working on course draft preview

--- src/components/course-draft-preview/Layout.tsx
+++ src/components/course-draft-preview/Layout.tsx
@@ -1,15 +1,32 @@
 import { Box, Container, Divider, Stack, Typography } from '@mui/material';
-import { useCurriculumFromParams } from '../../hooks/useCurriculum';
-import { ICurriculumSection, Lesson } from '../../features/courseDraftsSlice';
+import {
+  CourseDraft,
+  ICurriculumSection,
+  Lesson,
+} from '../../features/courseDraftsSlice';
 import { ReactNode, useEffect, useRef, useState } from 'react';
 import { BottomExtensionOpener } from '../curriculum/lecture/BottomExtensionOpener';
+import SearchIcon from '@mui/icons-material/Search';
+import { Star } from '../broad-courses-selection/Star';
+import { useCourseDraft } from '../../hooks/useCourseDraft';
+import { getSortedCopy } from '../drag-and-drop-v2/utils';
+import { formatDate, formatValue } from '../../utils/formatters';
+import EditCalendarIcon from '@mui/icons-material/EditCalendar';
 
 export const Layout = () => {
-  const { curriculum } = useCurriculumFromParams();
+  const courseDraft = useCourseDraft();
   const [currentLecture, setCurrentLecture] = useState<Lesson | null>(null);
 
+  const curriculum = courseDraft
+    ? getSortedCopy(courseDraft.courseContent.curriculum)
+    : [];
+
   useEffect(() => {
+    if (!courseDraft) return;
+
+    const curriculum = getSortedCopy(courseDraft.courseContent.curriculum);
     if (curriculum.length === 0) return;
+
     const firstLecture = curriculum[0].lessons[0];
     setCurrentLecture(firstLecture);
   }, []);
@@ -29,28 +46,113 @@ export const Layout = () => {
       >
         <Stack
           sx={{
-            bgcolor: 'background.default',
             width: '78%',
             flexDirection: 'column',
-            borderBottom: '1px solid',
-            borderTop: '1px solid',
-            borderColor: 'grey.400',
-            gap: 4,
           }}
         >
-          {currentLecture && currentLecture.video ? (
-            <VideoPlayer videoUrl={currentLecture.video.url} />
-          ) : currentLecture ? (
-            <LectureWithoutVideo lecture={currentLecture} />
-          ) : (
-            <VideoAreaLayout />
-          )}
+          <Box
+            sx={{
+              borderBottom: '1px solid',
+              borderColor: 'grey.400',
+              bgcolor: 'background.default',
+            }}
+          >
+            {currentLecture && currentLecture.video ? (
+              <VideoPlayer videoUrl={currentLecture.video.url} />
+            ) : currentLecture ? (
+              <LectureWithoutVideo lecture={currentLecture} />
+            ) : (
+              <VideoAreaLayout />
+            )}
+          </Box>
+          <Stack
+            sx={{
+              mt: 1,
+              gap: 1,
+            }}
+          >
+            <Stack
+              sx={{
+                flexDirection: 'row',
+                gap: 2,
+                px: 4,
+                py: 1,
+              }}
+            >
+              <Box
+                sx={{
+                  cursor: 'pointer',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <SearchIcon
+                  sx={{
+                    fontSize: 18,
+                  }}
+                />
+              </Box>
+              <Box
+                sx={{
+                  cursor: 'pointer',
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: 500,
+                  }}
+                >
+                  Overview
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  cursor: 'pointer',
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: 500,
+                  }}
+                >
+                  Notes
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  cursor: 'pointer',
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: 500,
+                  }}
+                >
+                  Announcements
+                </Typography>
+              </Box>
+            </Stack>
+            <Divider
+              sx={{
+                borderColor: 'grey.400',
+              }}
+            />
+            <Box
+              sx={{
+                mt: 2,
+              }}
+            >
+              {courseDraft && <Overview courseDraft={courseDraft} />}
+            </Box>
+          </Stack>
         </Stack>
         <Box
           sx={{
             width: '22%',
             border: '1px solid',
             borderRight: 'none',
+            borderTop: 'none',
             borderColor: 'grey.400',
           }}
         >
@@ -88,6 +190,155 @@ export const Layout = () => {
         </Box>
       </Stack>
     </Container>
+  );
+};
+
+interface OverviewProps {
+  courseDraft: CourseDraft;
+}
+
+const Overview = ({ courseDraft }: OverviewProps) => {
+  const ratings = courseDraft.ratings;
+  const ratingCount = formatValue(ratings.length);
+  const ratingSum = ratings.reduce((acc, { rating }) => acc + rating, 0);
+  const ratingAverage =
+    ratings.length > 0 ? (ratingSum / ratings.length).toFixed(1) : '0.0';
+  const studentCount = formatValue(courseDraft.enrollments.length);
+  const createdAt = formatDate(new Date(courseDraft.createdAt));
+
+  return (
+    <Stack
+      sx={{
+        gap: 1,
+      }}
+    >
+      <Stack
+        sx={{
+          flexDirection: 'row',
+          px: 4,
+          gap: 4,
+        }}
+      >
+        <Stack>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+            }}
+          >
+            <Typography
+              component={'div'}
+              sx={{
+                fontWeight: 500,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: 0.5,
+              }}
+            >
+              {ratingAverage}{' '}
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Star fillAmount={1} iconSize={18} />
+              </Box>
+            </Typography>
+          </Stack>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+            }}
+          >
+            <Typography variant="caption">{ratingCount} ratings</Typography>
+          </Stack>
+        </Stack>
+        <Stack>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+            }}
+          >
+            <Typography
+              sx={{
+                fontWeight: 500,
+              }}
+            >
+              {studentCount}
+            </Typography>
+          </Stack>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+            }}
+          >
+            <Typography variant="caption">Students</Typography>
+          </Stack>
+        </Stack>
+        {/** What total is this? */}
+        {/* <Stack>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+            }}
+          >
+            <Typography
+              sx={{
+                fontWeight: 500,
+              }}
+            >
+              0
+            </Typography>
+          </Stack>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+            }}
+          >
+            <Typography variant="caption">Total</Typography>
+          </Stack>
+        </Stack> */}
+      </Stack>
+      <Stack
+        sx={{
+          flexDirection: 'row',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        ></Box>
+      </Stack>
+      <Stack
+        sx={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          px: 4,
+          gap: 1,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            pb: 0.2,
+          }}
+        >
+          <EditCalendarIcon
+            sx={{
+              fontSize: 18,
+            }}
+          />
+        </Box>
+        <Typography variant="body2">Draft created on {createdAt}</Typography>
+      </Stack>
+    </Stack>
   );
 };
 
