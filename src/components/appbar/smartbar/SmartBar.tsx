4ae52e027170d73f41240cf93112754f542a1e08
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-11 11:50:22 +0300

    Removed old comments

--- src/components/appbar/smartbar/SmartBar.tsx
+++ src/components/appbar/smartbar/SmartBar.tsx
@@ -2,14 +2,11 @@ import { useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../../app/hooks';
 import { RedeemOfferCallToAction } from './RedeemOfferCallToAction';
 
-// Smart bar switches which call to action to show on top of app bar
-
 export const SmartBar = () => {
   const isRedeemed = useAppSelector((state) => state.specialOffer.isRedeemed);
   const isRejected = useAppSelector((state) => state.specialOffer.isRejected);
   const isExpired = useAppSelector((state) => state.specialOffer.isExpired);
 
-  // Show offer only on landing page
   const location = useLocation();
 
   const showSpecialOffer =
