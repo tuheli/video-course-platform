b2153805bf8844339836a75e5cd681210e3f9e90
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-11 17:09:51 +0300

    Added language data and modified state

--- src/features/languageSlice.ts
+++ src/features/languageSlice.ts
@@ -1,15 +1,85 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { Language } from '../../data/languageData';
+
+const languageCodes = {
+  english: 'en',
+  spanish: 'es',
+  french: 'fr',
+  german: 'de',
+  italian: 'it',
+  portuguese: 'pt',
+  finnish: 'fi',
+  dutch: 'nl',
+  chinese: 'zh',
+  japanese: 'ja',
+  korean: 'ko',
+  arabic: 'ar',
+} as const;
+
+export const languages = {
+  english: {
+    name: 'English',
+    code: languageCodes.english,
+  },
+  spanish: {
+    name: 'Spanish',
+    code: languageCodes.spanish,
+  },
+  french: {
+    name: 'French',
+    code: languageCodes.french,
+  },
+  german: {
+    name: 'German',
+    code: languageCodes.german,
+  },
+  italian: {
+    name: 'Italian',
+    code: languageCodes.italian,
+  },
+  portuguese: {
+    name: 'Portuguese',
+    code: languageCodes.portuguese,
+  },
+  finnish: {
+    name: 'Finnish',
+    code: languageCodes.finnish,
+  },
+  dutch: {
+    name: 'Dutch',
+    code: languageCodes.dutch,
+  },
+  chinese: {
+    name: 'Chinese',
+    code: languageCodes.chinese,
+  },
+  japanese: {
+    name: 'Japanese',
+    code: languageCodes.japanese,
+  },
+  korean: {
+    name: 'Korean',
+    code: languageCodes.korean,
+  },
+  arabic: {
+    name: 'Arabic',
+    code: languageCodes.arabic,
+  },
+} as const;
+
+export type Language = (typeof languages)[keyof typeof languages];
+
+type LanguageProperty = keyof typeof languages;
+
+export const getLanguage = (name: LanguageProperty) => {
+  return languages[name];
+};
 
 interface LanguageState {
-  language: Language;
+  currentLanguage: Language;
 }
 
 const initialState: LanguageState = {
-  language: {
-    text: 'English',
-    code: 'en',
-  },
+  currentLanguage: languages.english,
 };
 
 // TODO: Language state should be stored in local storage
@@ -19,7 +89,7 @@ const slice = createSlice({
   initialState,
   reducers: {
     changedLanguage: (state, action: PayloadAction<Language>) => {
-      state.language = action.payload;
+      state.currentLanguage = action.payload;
     },
   },
 });
