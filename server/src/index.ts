61ee2037061dbab7cd102f4bac10550a2028f994
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-12 17:22:15 +0300

    Added signup endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { connectToDatabase } from './database';
 import cors from 'cors';
+import signupRouter from './routers/signupRouter';
 
 const port = 3000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
+app.use('/api/signup', signupRouter);
 app.get('/api/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
