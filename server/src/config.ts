60b28fe3d5ee1f655c6438492417c6956d09025f
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-12 18:32:19 +0300

    Update config.ts

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,2 +1,33 @@
-export const DATABASE_URL =
-  'postgres://postgres:password@localhost:5432/postgres';
+import dotenv from 'dotenv';
+import { errorName } from './errorNames';
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL;
+const jwtSecret = process.env.JWT_SECRET;
+const port = process.env.PORT;
+
+if (!databaseUrl) {
+  const error = new Error(
+    'DATABASE_URL is missing from .env file. Define it and restart the server.'
+  );
+  error.name = errorName.missingEnvironmentVariable;
+  throw error;
+}
+
+if (!jwtSecret) {
+  const error = new Error(
+    'JWT_SECRET is missing from .env file. Define it and restart the server.'
+  );
+  error.name = errorName.missingEnvironmentVariable;
+  throw error;
+}
+
+if (!port) {
+  const error = new Error(
+    'PORT is missing from .env file. Define it and restart the server.'
+  );
+  error.name = errorName.missingEnvironmentVariable;
+  throw error;
+}
+
+export { databaseUrl, jwtSecret, port };
