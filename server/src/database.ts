38aa069b58d73f6a6ec04374369ce675cf870ce6
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-12 18:32:11 +0300

    Throwing error if config is faulty instead of exiting process

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,17 +1,30 @@
 import { Client } from 'pg';
-import { DATABASE_URL } from './config';
+import { databaseUrl } from './config';
+import { errorName } from './errorNames';
 
-const client = new Client(DATABASE_URL);
+const client = new Client(databaseUrl);
 
 const connectToDatabase = async () => {
   try {
     await client.connect();
   } catch (error) {
-    console.log(
-      'Error connecting to database. Is the database running? Error message:',
-      error
+    if (!(error instanceof Error)) {
+      const customError = new Error(
+        `Unknown error at connectToDatabase. Error object: ${error}`
+      );
+      customError.name = errorName.unknownError;
+      throw customError;
+    }
+
+    if (error.name === errorName.missingEnvironmentVariable) {
+      throw error;
+    }
+
+    const customError = new Error(
+      `Error connecting to database. Is the database running? Error message: ${error.message}`
     );
-    process.exit(1);
+    customError.name = errorName.errorAtDatabase;
+    throw customError;
   }
 };
 
@@ -19,8 +32,19 @@ const disconnectFromDatabase = async () => {
   try {
     await client.end();
   } catch (error) {
-    console.log('Error disconnecting from database. Error message:', error);
-    process.exit(1);
+    if (!(error instanceof Error)) {
+      const customError = new Error(
+        `Unknown error at disconnectFromDatabase. Error object: ${error}`
+      );
+      customError.name = errorName.unknownError;
+      throw customError;
+    }
+
+    const customError = new Error(
+      `Error disconnecting from database. Error message: ${error.message}`
+    );
+    customError.name = errorName.errorAtDatabase;
+    throw customError;
   }
 };
 
