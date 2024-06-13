9266c797c377bdecb73e84bba88d0ce470beda73
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-13 09:43:51 +0300

    Added navigation on click

--- client/src/components/appbar/LoginButton.tsx
+++ client/src/components/appbar/LoginButton.tsx
@@ -1,7 +1,12 @@
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginButton = () => {
-  const onClickLogin = () => {};
+  const navigate = useNavigate();
+
+  const onClickLogin = () => {
+    navigate('/login');
+  };
 
   return (
     <Button variant="outlined" onClick={onClickLogin}>
