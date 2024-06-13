b5ca1dd21fc4db060d3abeae5c4de19199554536
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-13 07:50:21 +0300

    Moved queries to their own file

--- server/src/routers/signupRouter.ts
+++ server/src/routers/signupRouter.ts
@@ -1,7 +1,8 @@
-import e, { Router } from 'express';
+import { Router } from 'express';
 import bcypt from 'bcrypt';
-import { client } from '../database';
 import { errorName } from '../errorNames';
+import { createUser } from '../queries/usersQueries';
+import { getUserByEmail } from '../queries/usersQueries';
 
 export interface CredentialsNotSafe {
   email: string;
@@ -22,9 +23,9 @@ interface UserInDatabaseNotSafe {
   receiveInsiderEmails: boolean;
 }
 
-type UserForDatabase = Omit<UserInDatabaseNotSafe, 'id'>;
+export type UserForDatabase = Omit<UserInDatabaseNotSafe, 'id'>;
 
-interface UserInDatabaseSafe {
+export interface UserInDatabaseSafe {
   id: string;
   email: string;
   fullName: string;
@@ -110,104 +111,6 @@ const toSignupRequestBody = (body: unknown): SignupRequestBody => {
   };
 
   return validRequest;
-};
-
-const createUser = async (
-  userForDatabase: UserForDatabase
-): Promise<UserInDatabaseSafe> => {
-  try {
-    const sqlText =
-      'INSERT INTO users (email, password_hash, full_name, receive_insider_emails) VALUES ($1, $2, $3, $4) RETURNING id, email, full_name, receive_insider_emails';
-
-    const values = [
-      userForDatabase.email,
-      userForDatabase.passwordHash,
-      userForDatabase.fullName,
-      userForDatabase.receiveInsiderEmails,
-    ];
-
-    const queryResult = await client.query(sqlText, values);
-
-    if (!queryResult || queryResult.rows.length !== 1) {
-      const customError = new Error(
-        `Failed to create user. Empty query result or row length is not 1.`
-      );
-      customError.name = errorName.errorAtDatabase;
-      throw customError;
-    }
-
-    const databaseRow = queryResult.rows[0];
-
-    const userInDatabaseSafe = {
-      id: databaseRow.id,
-      email: databaseRow.email,
-      fullName: databaseRow.full_name,
-      receiveInsiderEmails: databaseRow.receive_insider_emails,
-    };
-
-    return userInDatabaseSafe;
-  } catch (error) {
-    if (!(error instanceof Error)) {
-      const unknownError = new Error(
-        `Unknown error at createUser. Error object: ${error}`
-      );
-      unknownError.name = errorName.unknownError;
-      throw unknownError;
-    }
-
-    if (error.name === errorName.errorAtDatabase) {
-      const forwardedError = new Error(error.message);
-      forwardedError.name = error.name;
-      throw forwardedError;
-    }
-
-    const customError = new Error(
-      `Failed to create user. Error message: ${error.message}`
-    );
-    customError.name = errorName.errorAtDatabase;
-    throw customError;
-  }
-};
-
-const getUserByEmail = async (
-  email: string
-): Promise<UserInDatabaseSafe | null> => {
-  try {
-    const sqlText =
-      'SELECT id, email, full_name, receive_insider_emails FROM users WHERE email = $1';
-
-    const values = [email];
-    const queryResult = await client.query(sqlText, values);
-
-    if (!queryResult || queryResult.rows.length !== 1) {
-      return null;
-    }
-
-    const databaseRow = queryResult.rows[0];
-
-    const userInDatabaseSafe = {
-      id: databaseRow.id,
-      email: databaseRow.email,
-      fullName: databaseRow.full_name,
-      receiveInsiderEmails: databaseRow.receive_insider_emails,
-    };
-
-    return userInDatabaseSafe;
-  } catch (error) {
-    if (!(error instanceof Error)) {
-      const unknownError = new Error(
-        `Unknown error at getUser. Error object: ${error}`
-      );
-      unknownError.name = errorName.unknownError;
-      throw unknownError;
-    }
-
-    const customError = new Error(
-      `Failed to get user. Error message: ${error.message}`
-    );
-    customError.name = errorName.errorAtDatabase;
-    throw customError;
-  }
 };
 
 const router = Router();
