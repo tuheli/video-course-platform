a8d064140731e83147f777a5af09fc1405f0e379
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-13 14:59:27 +0300

    Sign in details can be saved to local storage

--- client/src/features/meSlice.ts
+++ client/src/features/meSlice.ts
@@ -1,33 +1,45 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { User } from './usersSlice';
 import { UserInDatabaseSafeWithToken } from './apiSlice';
 
 interface InitialState {
   user: UserInDatabaseSafeWithToken | null;
 }
 
-const devUser: User = {
-  credentials: {
-    email: 'elias.test@gmail.com',
-    password: '1234',
-  },
-  fullName: 'Elias Testington',
-  receiveInsiderEmails: true,
+export type SignedInPayload = UserInDatabaseSafeWithToken & {
+  staySignedIn?: boolean;
 };
 
 const initialState: InitialState = {
   user: null,
 };
 
+const saveSignedInUserToLocalStorage = (
+  userInDatabaseSafeWithToken: UserInDatabaseSafeWithToken
+) => {
+  localStorage.setItem(
+    'signedInUser',
+    JSON.stringify(userInDatabaseSafeWithToken)
+  );
+};
+
+const removeSignedInUserFromLocalStorage = () => {
+  localStorage.removeItem('signedInUser');
+};
+
 const slice = createSlice({
   name: 'me',
   initialState,
   reducers: {
-    signedIn: (state, action: PayloadAction<UserInDatabaseSafeWithToken>) => {
+    signedIn: (state, action: PayloadAction<SignedInPayload>) => {
       state.user = action.payload;
+
+      if (action.payload.staySignedIn) {
+        saveSignedInUserToLocalStorage(action.payload);
+      }
     },
     signedOut: (state) => {
       state.user = null;
+      removeSignedInUserFromLocalStorage();
     },
   },
 });
