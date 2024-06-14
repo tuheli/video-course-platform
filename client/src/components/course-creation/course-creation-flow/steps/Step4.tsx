390c5b43c5bfc675ef85082af6991acae4396d31
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-14 13:25:55 +0300

    Added notification component

--- client/src/components/course-creation/course-creation-flow/steps/Step4.tsx
+++ client/src/components/course-creation/course-creation-flow/steps/Step4.tsx
@@ -1,29 +1,33 @@
 import { Container, Stack, Typography } from '@mui/material';
 import { Step4Items } from './Step4Items';
+import { Notification } from '../../../notification/Notification';
 
 export const Step4 = () => {
   return (
-    <Container>
-      <Stack
-        sx={{
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: 4,
-        }}
-      >
-        <Typography variant="h4">
-          How much time can you spend creating your course per week?
-        </Typography>
-        <Typography
+    <>
+      <Notification />
+      <Container>
+        <Stack
           sx={{
-            mb: 4,
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 4,
           }}
         >
-          There's no wrong answer. We can help you achieve your goals even if
-          you don't have much time.
-        </Typography>
-        <Step4Items />
-      </Stack>
-    </Container>
+          <Typography variant="h4">
+            How much time can you spend creating your course per week?
+          </Typography>
+          <Typography
+            sx={{
+              mb: 4,
+            }}
+          >
+            There's no wrong answer. We can help you achieve your goals even if
+            you don't have much time.
+          </Typography>
+          <Step4Items />
+        </Stack>
+      </Container>
+    </>
   );
 };
