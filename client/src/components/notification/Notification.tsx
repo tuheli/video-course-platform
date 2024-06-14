2bcf2b47e52c72dfbd1f3b8596097090979ded64
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-14 10:48:15 +0300

    Update Notification.tsx
    - Notification should now start from "clean" state
    - It should not be open on render
    - It opens when notification message changes
    - On unmount the notification state is reset in store

--- client/src/components/notification/Notification.tsx
+++ client/src/components/notification/Notification.tsx
@@ -1,19 +1,29 @@
 import { Alert, Snackbar } from '@mui/material';
-import { useAppSelector } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { useEffect, useState } from 'react';
+import { removedNotification } from '../../features/notificationSlice';
 
 export const Notification = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { message, severity } = useAppSelector((state) => state.notification);
+  const dispatch = useAppDispatch();
 
   const onClose = () => {
     setIsOpen(false);
   };
 
   useEffect(() => {
-    if (!message) return;
-    setIsOpen(true);
+    if (message) {
+      setIsOpen(true);
+    }
   }, [message]);
+
+  useEffect(() => {
+    setIsOpen(false);
+    return () => {
+      dispatch(removedNotification());
+    };
+  }, []);
 
   return (
     <Snackbar
