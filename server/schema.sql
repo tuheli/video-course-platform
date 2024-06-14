01cc6402098c2609ac0be65cab98e801c5ca8db6
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-14 14:29:51 +0300

    Fixed syntax errors on schema

--- server/schema.sql
+++ server/schema.sql
@@ -12,6 +12,42 @@ CREATE TABLE users (
     receive_insider_emails BOOLEAN NOT NULL
 );
 
+CREATE TABLE coursedrafts (
+    id SERIAL PRIMARY KEY,
+    creator_email VARCHAR(255) NOT NULL,
+    course_type known_course_type NOT NULL,
+    course_title VARCHAR(60) NOT NULL,
+    course_category known_course_category NOT NULL,
+    creator_time_available_per_week time_available_per_week NOT NULL,
+    is_public BOOLEAN NOT NULL DEFAULT FALSE,
+    is_submission_process_completed BOOLEAN NOT NULL DEFAULT FALSE,
+    language VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE curriculums (
+    id SERIAL PRIMARY KEY,
+    course_draft_id INTEGER REFERENCES coursedrafts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE curriculum_sections (
+    id SERIAL PRIMARY KEY,
+    curriculum_id INTEGER REFERENCES curriculums(id) ON DELETE CASCADE,
+    title VARCHAR(160) NOT NULL,
+    learning_objective VARCHAR(160) NOT NULL,
+    order_index INTEGER NOT NULL
+);
+
+CREATE TABLE lessons (
+    id SERIAL PRIMARY KEY,
+    curriculum_section_id INTEGER REFERENCES curriculum_sections(id) ON DELETE CASCADE,
+    name VARCHAR(60) NOT NULL,
+    order_index INTEGER NOT NULL,
+    description VARCHAR(2000) NOT NULL,
+    video_url TEXT NOT NULL,
+    video_length_seconds INTEGER NOT NULL
+);
+
 CREATE TABLE course_ratings (
     id SERIAL PRIMARY KEY,
     course_draft_id INTEGER REFERENCES coursedrafts(id) ON DELETE CASCADE,
@@ -26,28 +62,6 @@ CREATE TABLE course_enrollments (
     enrollment_date TIMESTAMP NOT NULL
 );
 
-CREATE TABLE lessons (
-    id SERIAL PRIMARY KEY,
-    curriculum_section_id INTEGER REFERENCES curriculum_sections(id) ON DELETE CASCADE,
-    name VARCHAR(60) NOT NULL,
-    order_index INTEGER NOT NULL,
-    description VARCHAR(2000) NOT NULL,
-    video_url TEXT NOT NULL,
-    video_length_seconds INTEGER NOT NULL
-);
-
-CREATE TABLE curriculums (
-    id SERIAL PRIMARY KEY,
-    course_draft_id INTEGER REFERENCES coursedrafts(id) ON DELETE CASCADE,
-);
-
-CREATE TABLE curriculum_sections (
-    id SERIAL PRIMARY KEY,
-    curriculum_id INTEGER REFERENCES curriculums(id) ON DELETE CASCADE,
-    title VARCHAR(160) NOT NULL,
-    learning_objective VARCHAR(160) NOT NULL,
-    order_index INTEGER NOT NULL
-);
 
 CREATE TABLE learning_objectives (
     id SERIAL PRIMARY KEY,
@@ -76,19 +90,6 @@ CREATE TABLE course_contents (
     learning_objectives INTEGER REFERENCES learning_objectives(id) ON DELETE CASCADE,
     prerequisites INTEGER REFERENCES prerequisites(id) ON DELETE CASCADE,
     intended_learners INTEGER REFERENCES intended_learners(id) ON DELETE CASCADE,
-    curriculum INTEGER REFERENCES curriculum(id) ON DELETE CASCADE
-    video_content_length_seconds INTEGER NOT NULL,
-);
-
-CREATE TABLE coursedrafts (
-    id SERIAL PRIMARY KEY,
-    creator_email VARCHAR(255) NOT NULL,
-    course_type known_course_type NOT NULL,
-    course_title VARCHAR(60) NOT NULL,
-    course_category known_course_category NOT NULL,
-    creator_time_available_per_week time_available_per_week NOT NULL,
-    is_public BOOLEAN NOT NULL DEFAULT FALSE,
-    is_submission_process_completed BOOLEAN NOT NULL DEFAULT FALSE,
-    language VARCHAR(60) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    curriculum INTEGER REFERENCES curriculums(id) ON DELETE CASCADE,
+    video_content_length_seconds INTEGER NOT NULL
 );
