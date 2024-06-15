7697a0732da2357f460557e2690a732608130d31
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-15 13:28:01 +0300

    Added get course drafts by user id
    - Also updates on create course draft query

--- server/src/queries/courseDraftQueries.ts
+++ server/src/queries/courseDraftQueries.ts
@@ -10,6 +10,8 @@ import {
 
 interface CourseDraftInDatabase {
   id: number;
+  creatorId: number;
+  curriculumId: number;
   creatorEmail: string;
   courseType: string;
   courseTitle: string;
@@ -21,15 +23,47 @@ interface CourseDraftInDatabase {
   createdAt: string;
 }
 
+interface CurriculumInDatabase {
+  id: number;
+}
+
+type GetCourseDraftFromDatabaseResult = Omit<
+  CourseDraftInDatabase,
+  'id' | 'curriculumId'
+>;
+
 export const createCourseDraft = async (
   newCourseDraftEntry: NewCourseDraftEntry
 ): Promise<CourseDraft> => {
   try {
-    // 1. Row for course draft
+    await client.query('BEGIN;');
+
+    // 1. Row for curriculum
+    const sqlCurriculumText =
+      'INSERT INTO curriculums DEFAULT VALUES RETURNING id;';
+    const curriculumQueryResult = await client.query(sqlCurriculumText);
+
+    if (curriculumQueryResult.rowCount !== 1) {
+      const error = new Error(
+        'Returned row count is not 1 at create curriculum query.'
+      );
+      error.name = errorName.errorAtDatabase;
+      throw error;
+    }
+
+    const curriculumRow = curriculumQueryResult.rows[0];
+
+    const curriculumInDatabase: CurriculumInDatabase = {
+      id: curriculumRow.id,
+    };
+
+    // 2. Row for course draft
     const sqlCourseDraftText =
-      'INSERT INTO coursedrafts (creator_email, course_type, course_title, course_category, creator_time_available_per_week, is_public, is_submission_process_completed, language, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;';
+      'INSERT INTO coursedrafts (creator_id, curriculum_id, creator_email, course_type, course_title, course_category, creator_time_available_per_week, is_public, is_submission_process_completed, language, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id, creator_id, creator_email, course_type, course_title, course_category, creator_time_available_per_week, is_public, is_submission_process_completed, language, created_at;';
 
     const sqlCourseDraftValues = [
+      newCourseDraftEntry.creatorId,
+      curriculumInDatabase.id,
       newCourseDraftEntry.creatorEmail,
       newCourseDraftEntry.courseType,
       newCourseDraftEntry.courseTitle,
@@ -58,6 +92,8 @@ export const createCourseDraft = async (
 
     const createdCourseDraftInDatabase: CourseDraftInDatabase = {
       id: courseDraftRow.id,
+      curriculumId: curriculumInDatabase.id,
+      creatorId: courseDraftRow.creator_id,
       creatorEmail: courseDraftRow.creator_email,
       courseType: courseDraftRow.course_type,
       courseTitle: courseDraftRow.course_title,
@@ -71,22 +107,6 @@ export const createCourseDraft = async (
       createdAt: courseDraftRow.created_at,
     };
 
-    // 2. Row for curriculum
-    const sqlCurriculumText = 'INSERT INTO curriculums RETURNING id;';
-    const sqlCurriculumValues = [9];
-    const curriculumQueryResult = await client.query(
-      sqlCurriculumText,
-      sqlCurriculumValues
-    );
-
-    if (curriculumQueryResult.rowCount !== 1) {
-      const error = new Error(
-        'Returned row count is not 1 at create curriculum query.'
-      );
-      error.name = errorName.errorAtDatabase;
-      throw error;
-    }
-
     // NOTE: The course draft returned is structured
     // from the frontends perspective. For example,
     // in frontend the structure is used for
@@ -98,6 +118,7 @@ export const createCourseDraft = async (
 
     const createdCourseDraft: CourseDraft = {
       id: createdCourseDraftInDatabase.id,
+      creatorId: createdCourseDraftInDatabase.creatorId,
       creatorEmail: createdCourseDraftInDatabase.creatorEmail,
       courseType: createdCourseDraftInDatabase.courseType as CourseType,
       courseTitle: createdCourseDraftInDatabase.courseTitle,
@@ -130,8 +151,12 @@ export const createCourseDraft = async (
       createdAt: createdCourseDraftInDatabase.createdAt,
     };
 
+    await client.query('COMMIT;');
+
     return createdCourseDraft;
   } catch (error) {
+    await client.query('ROLLBACK;');
+
     if (!(error instanceof Error)) {
       const unknownError = new Error(
         'Unkonwn error at createCourseDraft. Error object is not an instance of Error.'
@@ -142,6 +167,81 @@ export const createCourseDraft = async (
 
     const newError = new Error(
       `Error at createCourseDraft. Error message: ${error.message}`
+    );
+    newError.name = errorName.errorAtDatabase;
+    throw newError;
+  }
+};
+
+export const getCourseDrafts = async (
+  userId: number
+): Promise<CourseDraft[]> => {
+  try {
+    // 1. Get course draft row
+    const selectCourseDraftQueryText =
+      'SELECT id, creator_id, creator_email, course_type, course_title, course_category, creator_time_available_per_week, is_public, is_submission_process_completed, language, created_at FROM coursedrafts WHERE creator_id = $1';
+
+    const selectCourseDraftQueryValues = [userId];
+
+    const selectCourseDraftQueryResult = await client.query(
+      selectCourseDraftQueryText,
+      selectCourseDraftQueryValues
+    );
+
+    const courseDraftRows = selectCourseDraftQueryResult.rows;
+
+    // NOTE: The structure is how the frontend
+    // needs the data.
+
+    // NOTE: Not fully implemented.
+    const courseDrafts: CourseDraft[] = courseDraftRows.map((row) => {
+      const courseDrafts: CourseDraft = {
+        id: row.id,
+        creatorId: row.creator_id,
+        creatorEmail: row.creator_email,
+        courseType: row.course_type,
+        courseTitle: row.course_title,
+        courseCategory: row.course_category,
+        creatorTimeAvailablePerWeek: row.creator_time_available_per_week,
+        isPublic: row.is_public,
+        isSubmissionProcessCompleted: row.is_submission_process_completed,
+        language: row.language,
+        courseContent: {
+          curriculum: [],
+          intendedLearners: {
+            items: [],
+            type: 'intendedLearners',
+          },
+          prerequisites: {
+            items: [],
+            type: 'prerequisites',
+          },
+          learningObjectives: {
+            items: [],
+            type: 'learningObjectives',
+          },
+          videoContentLengthSeconds: 0,
+        },
+        enrollments: [],
+        ratings: [],
+        createdAt: row.created_at,
+      };
+
+      return courseDrafts;
+    });
+
+    return courseDrafts;
+  } catch (error) {
+    if (!(error instanceof Error)) {
+      const unknownError = new Error(
+        'Unkonwn error at getCourseDraft. Error object is not an instance of Error.'
+      );
+      unknownError.name = errorName.unknownError;
+      throw unknownError;
+    }
+
+    const newError = new Error(
+      `Error at getCourseDraft. Error message: ${error.message}`
     );
     newError.name = errorName.errorAtDatabase;
     throw newError;
