beb5c03538aa7c05b9b9cecc465f0bbb6e4bc898
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-17 11:18:47 +0300

    Removed comment

--- client/src/components/instructor/MyCoursesOrJumpIntoCourseCreation.tsx
+++ client/src/components/instructor/MyCoursesOrJumpIntoCourseCreation.tsx
@@ -2,8 +2,6 @@ import { useGetCourseDraftsQuery } from '../../features/apiSlice';
 import { JumpIntoCourseCreation } from '../course-creation/JumpIntoCourseCreation';
 import { MyCourses } from './MyCourses';
 
-// TODO: Invalidate cache when a new course is created
-
 export const MyCoursesOrJumpIntoCourseCreation = () => {
   const { data, isLoading } = useGetCourseDraftsQuery();
 
