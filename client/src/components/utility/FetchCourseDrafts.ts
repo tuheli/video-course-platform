21a60e569c69cd39258105153e093b423fa9be7b
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-17 18:22:24 +0300

    Updated comment

--- client/src/components/utility/FetchCourseDrafts.ts
+++ client/src/components/utility/FetchCourseDrafts.ts
@@ -5,9 +5,13 @@ import { fetchedCourseDrafts } from '../../features/courseDraftsSlice';
 
 // NOTE: The course drafts are customized
 // locally and saved in the Redux store.
+// This component is a utility component
+// to initialize the local state.
 // I thought it could be good to update
 // the state locally and then send it
 // to the server on demand.
+// Also this approach helps to avoid
+// mixing the application and server logic.
 
 export const FetchCourseDrafts = () => {
   const { data } = useGetCourseDraftsQuery();
