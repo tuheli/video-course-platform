388d888a1b5abb427c6213bd8e49a1acd447164a
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-17 15:18:42 +0300

    Removed notification component
    - Moved notification component to sign up form

--- client/src/components/sign-up/SignUpSection.tsx
+++ client/src/components/sign-up/SignUpSection.tsx
@@ -1,11 +1,9 @@
 import { Box, Container } from '@mui/material';
 import { SignUpForm } from './SignUpForm';
-import { Notification } from '../notification/Notification';
 
 export const SignUpSection = () => {
   return (
     <>
-      <Notification />
       <Container>
         <Box
           sx={{
