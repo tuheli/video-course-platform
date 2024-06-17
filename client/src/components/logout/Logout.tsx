7abc13e68f050417b1557eef4b675a9245140c17
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-17 11:55:46 +0300

    Resetting api slice state on logout

--- client/src/components/logout/Logout.tsx
+++ client/src/components/logout/Logout.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from '../../app/hooks';
 import { signedOut } from '../../features/meSlice';
+import { apiSlice } from '../../features/apiSlice';
 
 export const Logout = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    dispatch(apiSlice.util.resetApiState());
     dispatch(signedOut());
   }, [dispatch]);
 
