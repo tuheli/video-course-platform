121ac5ba0b6f501e8f715053d9bdc6980ed8b1b4
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-17 15:20:08 +0300

    Updated notification

--- client/src/components/sign-in/SignInForm.tsx
+++ client/src/components/sign-in/SignInForm.tsx
@@ -1,12 +1,25 @@
-import { Box, Button, Divider, Paper, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Divider,
+  Paper,
+  Stack,
+  Typography,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 import { FormEvent, useState } from 'react';
 import { CredentialsNotSafe, useSigninMutation } from '../../features/apiSlice';
-import { isDataWithMessage, isObjectWithData } from '../../utils/apiUtils';
+import {
+  isDataWithMessage,
+  isFetchBaseQueryError,
+  isObjectWithData,
+} from '../../utils/apiUtils';
 import { useAppDispatch } from '../../app/hooks';
 import { signedIn } from '../../features/meSlice';
 import { TextInput } from '../sign-up/StyledTextInput';
 import { notified } from '../../features/notificationSlice';
+import { Notification } from '../notification/Notification';
 
 const validateEmail = (email: string) => {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -56,7 +69,7 @@ export const toCredentialsNotSafe = (
 
 export const SignInForm = () => {
   const [forceShowValidator, setForceShowValidation] = useState(false);
-  const [signin] = useSigninMutation();
+  const [signin, { isLoading }] = useSigninMutation();
   const dispatch = useAppDispatch();
 
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
@@ -85,6 +98,16 @@ export const SignInForm = () => {
 
       dispatch(signedIn(signedInPayload));
     } catch (error) {
+      if (isFetchBaseQueryError(error)) {
+        dispatch(
+          notified({
+            message: 'Fetch error occurred. Check internet connection.',
+            severity: 'error',
+          })
+        );
+        return;
+      }
+
       if (!isObjectWithData(error)) return;
       if (!isDataWithMessage(error.data)) return;
       dispatch(notified({ message: error.data.message, severity: 'info' }));
@@ -104,109 +127,149 @@ export const SignInForm = () => {
   };
 
   return (
-    <Paper
-      sx={{
-        position: 'relative',
-        width: 400,
-        p: 2,
-      }}
-    >
-      <Stack
+    <>
+      <Notification />
+      <Paper
         sx={{
-          flexDirection: 'column',
-          gap: 2,
+          position: 'relative',
+          width: 400,
+          p: 2,
         }}
       >
-        <Typography
-          sx={{
-            fontWeight: 600,
-          }}
-        >
-          Login to your Lorem account
-        </Typography>
         <Stack
-          component="form"
-          noValidate
           sx={{
             flexDirection: 'column',
-            gap: 1,
+            gap: 2,
           }}
-          onSubmit={onSubmit}
         >
-          <TextInput
-            placeholder="Email"
-            name="email"
-            validator={emailValidator}
-          />
-          <TextInput
-            placeholder="Password"
-            name="password"
-            type="password"
-            validator={passwordValidator}
-          />
-          <Box
+          <Typography
             sx={{
-              display: 'flex',
+              fontWeight: 600,
+            }}
+          >
+            Login to your Lorem account
+          </Typography>
+          <Stack
+            component="form"
+            noValidate
+            sx={{
+              flexDirection: 'column',
               gap: 1,
-              my: 1,
             }}
+            onSubmit={onSubmit}
           >
-            <div>
-              <input type="checkbox" name="stay-logged-in-on-this-device" />
-            </div>
-            <label htmlFor="stay-logged-in-on-this-device">
-              Stay logged in on this device
-            </label>
-          </Box>
-          <Button variant="contained" color="secondary" type="submit">
-            Log in
-          </Button>
-        </Stack>
-        <Typography
-          variant="body2"
-          sx={{
-            textAlign: 'center',
-          }}
-        >
-          or{' '}
-          <Link
-            to="/"
-            style={{
-              textDecoration: 'none',
-            }}
-          >
-            <Typography
-              component="span"
-              variant="body2"
+            <TextInput
+              placeholder="Email"
+              name="email"
+              validator={emailValidator}
+            />
+            <TextInput
+              placeholder="Password"
+              name="password"
+              type="password"
+              validator={passwordValidator}
+            />
+            <Box
               sx={{
-                textDecoration: 'underline',
-                textUnderlineOffset: 3,
-                textDecorationColor: 'secondary.dark',
-                color: 'secondary.dark',
-                fontWeight: 600,
+                display: 'flex',
+                gap: 1,
+                my: 1,
               }}
             >
-              Forgot password
-            </Typography>
-          </Link>{' '}
-        </Typography>
-        <Divider />
-        <Stack>
+              <div>
+                <input type="checkbox" name="stay-logged-in-on-this-device" />
+              </div>
+              <label htmlFor="stay-logged-in-on-this-device">
+                Stay logged in on this device
+              </label>
+            </Box>
+            <Button
+              disabled={isLoading}
+              variant="contained"
+              color="secondary"
+              type="submit"
+            >
+              Log in
+              {isLoading && (
+                <Box
+                  sx={{
+                    position: 'absolute',
+                    right: 8,
+                  }}
+                >
+                  <CircularProgress size={22} />
+                </Box>
+              )}
+            </Button>
+          </Stack>
           <Typography
             variant="body2"
             sx={{
               textAlign: 'center',
             }}
           >
-            Don't have an account?{' '}
+            or{' '}
             <Link
-              to="/signup"
+              to="/"
               style={{
                 textDecoration: 'none',
               }}
             >
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationColor: 'secondary.dark',
+                  color: 'secondary.dark',
+                  fontWeight: 600,
+                }}
+              >
+                Forgot password
+              </Typography>
+            </Link>{' '}
+          </Typography>
+          <Divider />
+          <Stack>
+            <Typography
+              variant="body2"
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              Don't have an account?{' '}
+              <Link
+                to="/signup"
+                style={{
+                  textDecoration: 'none',
+                }}
+              >
+                {' '}
+                <Typography
+                  component="span"
+                  sx={{
+                    color: 'secondary.dark',
+                    fontWeight: 600,
+                    textDecoration: 'underline',
+                    textUnderlineOffset: 3,
+                    textDecorationColor: 'secondary.dark',
+                  }}
+                >
+                  Sign up
+                </Typography>
+              </Link>
+            </Typography>
+            <Link
+              to="/"
+              style={{
+                textDecoration: 'none',
+                textAlign: 'center',
+              }}
+            >
               {' '}
               <Typography
+                variant="body2"
                 component="span"
                 sx={{
                   color: 'secondary.dark',
@@ -216,34 +279,12 @@ export const SignInForm = () => {
                   textDecorationColor: 'secondary.dark',
                 }}
               >
-                Sign up
+                Log in with your organization
               </Typography>
             </Link>
-          </Typography>
-          <Link
-            to="/"
-            style={{
-              textDecoration: 'none',
-              textAlign: 'center',
-            }}
-          >
-            {' '}
-            <Typography
-              variant="body2"
-              component="span"
-              sx={{
-                color: 'secondary.dark',
-                fontWeight: 600,
-                textDecoration: 'underline',
-                textUnderlineOffset: 3,
-                textDecorationColor: 'secondary.dark',
-              }}
-            >
-              Log in with your organization
-            </Typography>
-          </Link>
+          </Stack>
         </Stack>
-      </Stack>
-    </Paper>
+      </Paper>
+    </>
   );
 };
