2f7d3a351c692b0b074e4f5729c831b89bb06730
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-26 15:28:19 +0300

    Lesson descriptions are searched from local storage

--- client/src/hooks/useSaveCurriculum.ts
+++ client/src/hooks/useSaveCurriculum.ts
@@ -1,5 +1,13 @@
+import {
+  getLectureDescriptionLocalStorageKey,
+  getStringSlateEditorStateFromLocalStorageOrDefault,
+} from '../components/text-editor/utils';
 import { useUpdateCurriculumSectionsMutation } from '../features/apiSlice';
 import { CourseDraft } from '../features/courseDraftsSlice';
+
+// NOTE: Lesson description is rich text
+// and stored in localstorage so its
+// not found from redux store.
 
 export const useSaveCurriculum = () => {
   const [updateCurriculumSections] = useUpdateCurriculumSectionsMutation();
@@ -9,28 +17,39 @@ export const useSaveCurriculum = () => {
     try {
       const entries = courseDraft.courseContent.curriculum.map(
         (section: any) => {
-          const entry = {
+          const sectionEntry = {
             id: section.id,
             title: section.title,
             learningObjective: section.learningObjective,
             orderIndex: section.orderIndex,
             lessons: section.lessons.map((lesson: any) => {
+              const descriptionKey = getLectureDescriptionLocalStorageKey(
+                courseDraft.id,
+                section.id,
+                lesson.id
+              );
+
+              const description =
+                getStringSlateEditorStateFromLocalStorageOrDefault(
+                  descriptionKey
+                );
+
               return {
                 id: lesson.id,
                 name: lesson.name,
-                description: lesson.description,
+                description,
                 orderIndex: lesson.orderIndex,
               };
             }),
           };
-          return entry;
+          return sectionEntry;
         }
       );
 
       await updateCurriculumSections({
         courseDraftId: courseDraft.id,
         entries,
-      });
+      }).unwrap();
     } catch (error) {
       throw error;
     }
