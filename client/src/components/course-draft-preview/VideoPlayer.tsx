91de4e8ec9f8af252603873addba885df9dfbcc4
tuheli <94482031+tuheli@users.noreply.github.com>
2024-06-28 13:58:19 +0300

    Working on receiving video from server

--- client/src/components/course-draft-preview/VideoPlayer.tsx
+++ client/src/components/course-draft-preview/VideoPlayer.tsx
@@ -1,17 +1,46 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { VideoAreaLayout } from './VideoAreaLayout';
+import { Lesson } from '../../features/courseDraftsSlice';
+import { useCreateLessonVideostreamTokenMutation } from '../../features/apiSlice';
 
 interface VideoPlayerProps {
-  videoUrl: string;
+  currentLecture: Lesson;
 }
 
-export const VideoPlayer = ({ videoUrl }: VideoPlayerProps) => {
+export const VideoPlayer = ({ currentLecture }: VideoPlayerProps) => {
+  const [src, setSrc] = useState<string>('');
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [createToken, { data }] = useCreateLessonVideostreamTokenMutation();
 
   useEffect(() => {
     if (!videoRef.current) return;
     videoRef.current.load();
-  }, [videoUrl]);
+  }, [currentLecture, src]);
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+
+    const getVideoStreamToken = async () => {
+      try {
+        const response = await createToken({ lectureId: currentLecture.id });
+
+        if (response.data) {
+          const token = response.data.token;
+          setSrc(
+            `http://localhost:3000/api/coursedrafts/videostream/${currentLecture.id}?token=${token}`
+          );
+        }
+      } catch (error) {
+        throw error;
+      }
+    };
+
+    try {
+      getVideoStreamToken();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [currentLecture]);
 
   return (
     <VideoAreaLayout>
@@ -28,7 +57,7 @@ export const VideoPlayer = ({ videoUrl }: VideoPlayerProps) => {
           backgroundColor: 'currentcolor',
         }}
       >
-        <source src={videoUrl} />
+        <source src={src} />
       </video>
     </VideoAreaLayout>
   );
