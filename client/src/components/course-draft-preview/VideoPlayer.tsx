518b976df8cd10d1ad431ea09f9fe9bc171d4f18
tuheli <94482031+tuheli@users.noreply.github.com>
2024-07-08 15:04:05 +0300

    Video url comes from aws s3

--- client/src/components/course-draft-preview/VideoPlayer.tsx
+++ client/src/components/course-draft-preview/VideoPlayer.tsx
@@ -1,46 +1,30 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { VideoAreaLayout } from './VideoAreaLayout';
-import { Lesson } from '../../features/courseDraftsSlice';
-import { useCreateLessonVideostreamTokenMutation } from '../../features/apiSlice';
+import { useGetVideoUrlQuery } from '../../features/apiSlice';
 
 interface VideoPlayerProps {
-  currentLecture: Lesson;
+  coursedraftId: number;
+  sectionId: number;
+  lectureId: number;
 }
 
-export const VideoPlayer = ({ currentLecture }: VideoPlayerProps) => {
-  const [src, setSrc] = useState<string>('');
+export const VideoPlayer = ({
+  coursedraftId,
+  sectionId,
+  lectureId,
+}: VideoPlayerProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [createToken] = useCreateLessonVideostreamTokenMutation();
+  const { data, isError } = useGetVideoUrlQuery({
+    coursedraftId,
+    sectionId,
+    lectureId,
+  });
+  const videoUrl = data && !isError ? data.presignedUrl : '';
 
   useEffect(() => {
     if (!videoRef.current) return;
     videoRef.current.load();
-  }, [currentLecture, src]);
-
-  useEffect(() => {
-    if (!videoRef.current) return;
-
-    const getVideoStreamToken = async () => {
-      try {
-        const response = await createToken({ lectureId: currentLecture.id });
-
-        if (response.data) {
-          const token = response.data.token;
-          setSrc(
-            `http://localhost:3000/api/coursedrafts/videostream/${currentLecture.id}?token=${token}`
-          );
-        }
-      } catch (error) {
-        throw error;
-      }
-    };
-
-    try {
-      getVideoStreamToken();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [currentLecture]);
+  }, [videoUrl, isError]);
 
   return (
     <VideoAreaLayout>
@@ -57,7 +41,7 @@ export const VideoPlayer = ({ currentLecture }: VideoPlayerProps) => {
           backgroundColor: 'currentcolor',
         }}
       >
-        <source src={src} />
+        <source src={videoUrl} />
       </video>
     </VideoAreaLayout>
   );
