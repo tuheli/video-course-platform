52fc47c50cd883e304cb58c13122184f877bb3c9
tuheli <elias.tuhola@gmail.com>
2024-07-13 22:54:09 +0300

    Added comment
    - Theres probably a bunch of similiar pieces of code in the project

--- client/src/components/instructor/AreYouReadyToBeginOrEmpty.tsx
+++ client/src/components/instructor/AreYouReadyToBeginOrEmpty.tsx
@@ -5,6 +5,14 @@ import { AreYouReadyToBegin } from './AreYouReadyToBegin';
 export const AreYouReadyToBeginOrEmpty = () => {
   const signedInUser = useAppSelector((state) => state.userState.user);
 
+  // This next thing is a bit weird.
+  // It was done early on when I did the frontend
+  // but it just checks if the user has any courses.
+  // Nowdays I would write things like this a bit
+  // more readable maybe using extra boolean like
+  // doesUserHaveAnyCourses or something like that and
+  // then like
+  // isAreYouReadyToBeginVisible = isUserSignedIn && doesUserHaveAnyCourses etc.
   const isAreYouReadyToBeginVisible =
     useAppSelector((state) => state.courseDrafts).filter(
       ({ creatorEmail }) => signedInUser?.email === creatorEmail
