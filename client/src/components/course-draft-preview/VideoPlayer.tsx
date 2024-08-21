be90609e54fc964a8f5a156e15d8d7c807f0d8bf
tuheli <elias.tuhola@gmail.com>
2024-08-21 20:33:50 +0300

    Video is shown conditionally when video url exists
    - Also clearing the video preview state on error

--- client/src/components/course-draft-preview/VideoPlayer.tsx
+++ client/src/components/course-draft-preview/VideoPlayer.tsx
@@ -6,12 +6,14 @@ interface VideoPlayerProps {
   coursedraftId: number;
   sectionId: number;
   lectureId: number;
+  clearVideoPreviewState: () => void;
 }
 
 export const VideoPlayer = ({
   coursedraftId,
   sectionId,
   lectureId,
+  clearVideoPreviewState,
 }: VideoPlayerProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const { data, isError } = useGetVideoUrlQuery({
@@ -20,29 +22,37 @@ export const VideoPlayer = ({
     lectureId,
   });
   const videoUrl = data && !isError ? data.presignedUrl : '';
+  const isVideoVisible = Boolean(videoUrl);
 
   useEffect(() => {
     if (!videoRef.current) return;
     videoRef.current.load();
   }, [videoUrl, isError]);
 
+  useEffect(() => {
+    if (!isError) return;
+    clearVideoPreviewState();
+  }, [isError]);
+
   return (
     <VideoAreaLayout>
-      <video
-        ref={videoRef}
-        controls
-        controlsList="nodownload"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'currentcolor',
-        }}
-      >
-        <source src={videoUrl} />
-      </video>
+      {isVideoVisible && (
+        <video
+          ref={videoRef}
+          controls
+          controlsList="nodownload"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'currentcolor',
+          }}
+        >
+          <source src={videoUrl} />
+        </video>
+      )}
     </VideoAreaLayout>
   );
 };
