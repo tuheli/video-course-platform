04e504d3692bd3c5e1f285e6f039ec60fead8a60
tuheli <elias.tuhola@gmail.com>
2024-08-21 20:35:13 +0300

    Added back button

--- client/src/components/course-draft-preview/CourseDraftPreviewPage.tsx
+++ client/src/components/course-draft-preview/CourseDraftPreviewPage.tsx
@@ -1,11 +1,17 @@
-import { AppBar, Box, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, Toolbar } from '@mui/material';
 import { useScrollToTop } from '../../hooks/useScrollToTop';
 import { Footer } from '../footer/Footer';
 import { Layout } from './Layout';
 import { CompanyLogo } from '../appbar/CompanyLogo';
+import { useNavigate } from 'react-router-dom';
 
 export const CourseDraftPreviewPage = () => {
   useScrollToTop();
+  const navigate = useNavigate();
+
+  const onClickBack = () => {
+    navigate(-1);
+  };
 
   return (
     <Box
@@ -24,9 +30,20 @@ export const CourseDraftPreviewPage = () => {
           sx={{
             boxShadow: 'none',
             bgcolor: 'background.dark',
+            justifyContent: 'space-between',
           }}
         >
           <CompanyLogo isWhite={true} />
+          <Button
+            onClick={onClickBack}
+            variant="outlined"
+            color="primary"
+            sx={{
+              width: 80,
+            }}
+          >
+            Back
+          </Button>
         </Toolbar>
       </AppBar>
       <Box>
