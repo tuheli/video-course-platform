538ef9f2a5261ccd227b3d5faefd239185b8a8e7
tuheli <elias.tuhola@gmail.com>
2024-08-22 09:30:09 +0300

    Created request logger

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -3,6 +3,17 @@ import { jwtSecret } from './config';
 import { errorName } from './errorNames';
 import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 import { getUserById } from './queries/usersQueries';
+import { UserInDatabaseSafe } from './types';
+
+interface RequestLogInfo {
+  timestamp: string;
+  url: string;
+  method?: string;
+  body?: unknown;
+  ip?: string;
+  user: UserInDatabaseSafe | null;
+  authorization?: string;
+}
 
 export const errorHandler = (
   error: Error,
@@ -94,6 +105,132 @@ export const userExtractor = async (
     }
 
     req.user = userInDatabaseSafe;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const requestLogger = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const replaceSensitiveInformationInBody = (body: unknown) => {
+    if (!body || typeof body !== 'object') return body;
+    if (
+      !('credentialsNotSafe' in body) ||
+      !body.credentialsNotSafe ||
+      typeof body.credentialsNotSafe !== 'object'
+    )
+      return body;
+    if (
+      !('password' in body.credentialsNotSafe) ||
+      !body.credentialsNotSafe.password ||
+      typeof body.credentialsNotSafe.password !== 'string'
+    )
+      return body;
+
+    const credentialsNotSafe = {
+      ...body.credentialsNotSafe,
+      password: '****',
+    };
+
+    const nonSensitiveBody = {
+      ...body,
+      credentialsNotSafe,
+    };
+
+    return nonSensitiveBody;
+  };
+
+  const getUserFromRequest = async (
+    req: Request
+  ): Promise<UserInDatabaseSafe | null> => {
+    try {
+      if (!jwtSecret) {
+        const error = new Error(
+          'Configuration missing jwt secret from .env file.'
+        );
+        error.name = errorName.missingEnvironmentVariable;
+        throw error;
+      }
+
+      const authorization = req.get('Authorization');
+
+      if (authorization && authorization.startsWith('Bearer ')) {
+        req.authorizationToken = authorization.replace('Bearer ', '');
+      }
+      if (!req.authorizationToken) return null;
+
+      const decodedToken = jwt.verify(
+        req.authorizationToken,
+        jwtSecret
+      ) as jwt.JwtPayload;
+      if (!decodedToken.id) return null;
+
+      const userInDatabaseSafe = await getUserById(decodedToken.id);
+      if (!userInDatabaseSafe) undefined;
+
+      return userInDatabaseSafe;
+    } catch (error) {
+      return null;
+    }
+  };
+
+  try {
+    const isRequestToApiEndpoint = req.url.startsWith('/api/');
+    if (!isRequestToApiEndpoint) next();
+
+    const timestamp = new Date().toISOString();
+    const ip = req.ip;
+    const url = req.url;
+    const method = req.method;
+    const body = replaceSensitiveInformationInBody(req.body);
+    const user = await getUserFromRequest(req);
+    const authorization = req.headers.authorization;
+
+    const info: RequestLogInfo = {
+      timestamp,
+      url,
+      method,
+      body,
+      ip,
+      user,
+      authorization,
+    };
+
+    switch (req.method) {
+      case 'GET':
+        info.method = 'GET';
+        break;
+      case 'HEAD':
+        info.method = 'HEAD';
+        break;
+      case 'POST':
+        info.method = 'POST';
+        break;
+      case 'PUT':
+        info.method = 'PUT';
+        break;
+      case 'DELETE':
+        info.method = 'DELETE';
+        break;
+      case 'CONNECT':
+        info.method = 'CONNECT';
+        break;
+      case 'OPTIONS':
+        info.method = 'OPTIONS';
+        break;
+      case 'TRACE':
+        info.method = 'TRACE';
+        break;
+      case 'PATCH':
+        info.method = 'PATCH';
+        break;
+    }
+
+    console.log(info);
     next();
   } catch (error) {
     next(error);
