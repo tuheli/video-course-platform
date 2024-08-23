4813eed5e1935e5e437dddf4d57b00caec52b49b
tuheli <elias.tuhola@gmail.com>
2024-08-23 11:21:44 +0300

    Added explicit return

--- server/src/routers/topSecretDemovideoRouter.ts
+++ server/src/routers/topSecretDemovideoRouter.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/', async (req, res, next) => {
   try {
     const presignedUrl = await getPresignedTopSecretDemovideoUrl();
-    res.status(200).json({ presignedUrl });
+    return res.status(200).json({ presignedUrl });
   } catch (error) {
     next(error);
   }
